f9da439a4394fce4f3806f0ba784722347e23a7a
hsluoyz <hsluoyz@qq.com>
2015-07-17 14:55:17 +0800

    Use macros to control the driver name and deploy folder name.

--- installer/Deploy.bat
+++ installer/Deploy.bat
@@ -1,30 +1,33 @@
 
+set DRIVER_NAME=npcap
+set DEPLOY_FOLDER_NAME=win7_above
+set VS_CONFIG_MODE=
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Copy Npcap driver files
-xcopy /Y	"..\packetWin7\npf\Win7Release\npf Package\npcap.cat"						.\win7_above\x86
-xcopy /Y	"..\packetWin7\npf\Win7Release\npf Package\npcap.inf"						.\win7_above\x86
-xcopy /Y	"..\packetWin7\npf\Win7Release\npf Package\npcap.sys"						.\win7_above\x86
-xcopy /Y	"..\packetWin7\npf\x64\Win7Release\npf Package\npcap.cat"					.\win7_above\x64
-xcopy /Y	"..\packetWin7\npf\x64\Win7Release\npf Package\npcap.inf"					.\win7_above\x64
-xcopy /Y	"..\packetWin7\npf\x64\Win7Release\npf Package\npcap.sys"					.\win7_above\x64
-xcopy /Y	"..\packetWin7\npf\Win7ReleaseAdmin-onlyMode\npf Package\npcap.cat"			.\win7_above\x86\admin_only
-xcopy /Y	"..\packetWin7\npf\Win7ReleaseAdmin-onlyMode\npf Package\npcap.inf"			.\win7_above\x86\admin_only
-xcopy /Y	"..\packetWin7\npf\Win7ReleaseAdmin-onlyMode\npf Package\npcap.sys"			.\win7_above\x86\admin_only
-xcopy /Y	"..\packetWin7\npf\x64\Win7ReleaseAdmin-onlyMode\npf Package\npcap.cat"		.\win7_above\x64\admin_only
-xcopy /Y	"..\packetWin7\npf\x64\Win7ReleaseAdmin-onlyMode\npf Package\npcap.inf"		.\win7_above\x64\admin_only
-xcopy /Y	"..\packetWin7\npf\x64\Win7ReleaseAdmin-onlyMode\npf Package\npcap.sys"		.\win7_above\x64\admin_only
+xcopy /Y	"..\packetWin7\npf\Win7Release\npf Package\%DRIVER_NAME%.cat"						.\%DEPLOY_FOLDER_NAME%\x86
+xcopy /Y	"..\packetWin7\npf\Win7Release\npf Package\%DRIVER_NAME%.inf"						.\%DEPLOY_FOLDER_NAME%\x86
+xcopy /Y	"..\packetWin7\npf\Win7Release\npf Package\%DRIVER_NAME%.sys"						.\%DEPLOY_FOLDER_NAME%\x86
+xcopy /Y	"..\packetWin7\npf\x64\Win7Release\npf Package\%DRIVER_NAME%.cat"					.\%DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\npf\x64\Win7Release\npf Package\%DRIVER_NAME%.inf"					.\%DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\npf\x64\Win7Release\npf Package\%DRIVER_NAME%.sys"					.\%DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\npf\Win7ReleaseAdmin-onlyMode\npf Package\%DRIVER_NAME%.cat"			.\%DEPLOY_FOLDER_NAME%\x86\admin_only
+xcopy /Y	"..\packetWin7\npf\Win7ReleaseAdmin-onlyMode\npf Package\%DRIVER_NAME%.inf"			.\%DEPLOY_FOLDER_NAME%\x86\admin_only
+xcopy /Y	"..\packetWin7\npf\Win7ReleaseAdmin-onlyMode\npf Package\%DRIVER_NAME%.sys"			.\%DEPLOY_FOLDER_NAME%\x86\admin_only
+xcopy /Y	"..\packetWin7\npf\x64\Win7ReleaseAdmin-onlyMode\npf Package\%DRIVER_NAME%.cat"		.\%DEPLOY_FOLDER_NAME%\x64\admin_only
+xcopy /Y	"..\packetWin7\npf\x64\Win7ReleaseAdmin-onlyMode\npf Package\%DRIVER_NAME%.inf"		.\%DEPLOY_FOLDER_NAME%\x64\admin_only
+xcopy /Y	"..\packetWin7\npf\x64\Win7ReleaseAdmin-onlyMode\npf Package\%DRIVER_NAME%.sys"		.\%DEPLOY_FOLDER_NAME%\x64\admin_only
 
 :: Copy Packet.dll
-xcopy /Y	"..\packetWin7\Dll\Project\Release No NetMon and AirPcap\Packet.dll"		.\win7_above\x86
-xcopy /Y	"..\packetWin7\Dll\Project\x64\Release No NetMon and AirPcap\Packet.dll"	.\win7_above\x64
+xcopy /Y	"..\packetWin7\Dll\Project\Release No NetMon and AirPcap%VS_CONFIG_MODE%\Packet.dll"		.\%DEPLOY_FOLDER_NAME%\x86
+xcopy /Y	"..\packetWin7\Dll\Project\x64\Release No NetMon and AirPcap%VS_CONFIG_MODE%\Packet.dll"	.\%DEPLOY_FOLDER_NAME%\x64
 
 :: Copy NPFInstall.exe
-xcopy /Y	"..\packetWin7\NPFInstall\Release\NPFInstall.exe"							.\win7_above\x86
-xcopy /Y	"..\packetWin7\NPFInstall\x64\Release\NPFInstall.exe"						.\win7_above\x64
+xcopy /Y	"..\packetWin7\NPFInstall\Release%VS_CONFIG_MODE%\NPFInstall.exe"							.\%DEPLOY_FOLDER_NAME%\x86
+xcopy /Y	"..\packetWin7\NPFInstall\x64\Release%VS_CONFIG_MODE%\NPFInstall.exe"						.\%DEPLOY_FOLDER_NAME%\x64
 
 :: Copy NPcapHelper.exe
-xcopy /Y	"..\packetWin7\Helper\release\NPcapHelper.exe"								.\win7_above\x86\admin_only
-xcopy /Y	"..\packetWin7\Helper\x64\release\NPcapHelper.exe"							.\win7_above\x64\admin_only
+xcopy /Y	"..\packetWin7\Helper\release\NPcapHelper.exe"								.\%DEPLOY_FOLDER_NAME%\x86\admin_only
+xcopy /Y	"..\packetWin7\Helper\x64\release\NPcapHelper.exe"							.\%DEPLOY_FOLDER_NAME%\x64\admin_only
 
 :: Npcap uses the original WinPcap wpcap.dll with exactly the same code, we just changed the version number.
 :: Copy wpcap.dll
@@ -36,26 +39,26 @@ xcopy /Y	"..\wpcap\PRJ\Release No AirPcap\x64\wpcap.dll"								.\x64
 :: put the private key string (e.g. 123456) to environment variable %NPF_SIGN_PK%
 
 :: Sign the driver
-:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x86\npcap.sys
-:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x86\npcap.cat
-:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x64\npcap.sys
-:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x64\npcap.cat
-:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x86\admin_only\npcap.sys
-:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x86\admin_only\npcap.cat
-:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x64\admin_only\npcap.sys
-:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x64\admin_only\npcap.cat
+:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x86\%DRIVER_NAME%.sys
+:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x86\%DRIVER_NAME%.cat
+:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x64\%DRIVER_NAME%.sys
+:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x64\%DRIVER_NAME%.cat
+:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x86\admin_only\%DRIVER_NAME%.sys
+:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x86\admin_only\%DRIVER_NAME%.cat
+:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x64\admin_only\%DRIVER_NAME%.sys
+:: signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x64\admin_only\%DRIVER_NAME%.cat
 
 :: Sign Packet.dll
-signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x86\Packet.dll
-signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x64\Packet.dll
+signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x86\Packet.dll
+signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x64\Packet.dll
 
 :: Sign NPFInstall.exe
-signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x86\NPFInstall.exe
-signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x64\NPFInstall.exe
+signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x86\NPFInstall.exe
+signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x64\NPFInstall.exe
 
 :: Sign NPcapHelper.exe
-signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x86\admin_only\NPcapHelper.exe
-signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\win7_above\x64\admin_only\NPcapHelper.exe
+signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x86\admin_only\NPcapHelper.exe
+signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\%DEPLOY_FOLDER_NAME%\x64\admin_only\NPcapHelper.exe
 
 :: Sign wpcap.dll
 signtool sign /f %NPF_CERT_PATH% /p %NPF_SIGN_PK% .\wpcap.dll
