5109a40f629fe236b71f531fda9afd8e978425ba
hsluoyz <hsluoyz@qq.com>
2016-02-17 12:54:23 +0800

    Improved the deploy scripts, so no need to manually create the deploy folders any more.

--- installer/Deploy_WinPcap.bat
+++ installer/Deploy_WinPcap.bat
@@ -5,40 +5,40 @@ set VISTA_DEPLOY_FOLDER_NAME=vista_winpcap
 set VS_CONFIG_MODE=(WinPcap Mode)
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Copy Npcap driver files
-xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"						.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"						.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"					.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"						.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"					.\%DEPLOY_FOLDER_NAME%\x64
-xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"					.\%DEPLOY_FOLDER_NAME%\x64
-xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"				.\%DEPLOY_FOLDER_NAME%\x64
-xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"					.\%DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"						.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"						.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"					.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"						.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"					.\%DEPLOY_FOLDER_NAME%\x64\
+xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"					.\%DEPLOY_FOLDER_NAME%\x64\
+xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"				.\%DEPLOY_FOLDER_NAME%\x64\
+xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"					.\%DEPLOY_FOLDER_NAME%\x64\
 
-xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"						.\%VISTA_DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"						.\%VISTA_DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"					.\%VISTA_DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"						.\%VISTA_DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"					.\%VISTA_DEPLOY_FOLDER_NAME%\x64
-xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"					.\%VISTA_DEPLOY_FOLDER_NAME%\x64
-xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"				.\%VISTA_DEPLOY_FOLDER_NAME%\x64
-xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"					.\%VISTA_DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"						.\%VISTA_DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"						.\%VISTA_DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"					.\%VISTA_DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"						.\%VISTA_DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"					.\%VISTA_DEPLOY_FOLDER_NAME%\x64\
+xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"					.\%VISTA_DEPLOY_FOLDER_NAME%\x64\
+xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"				.\%VISTA_DEPLOY_FOLDER_NAME%\x64\
+xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"					.\%VISTA_DEPLOY_FOLDER_NAME%\x64\
 
 :: Copy Packet.dll
-xcopy /Y	"..\packetWin7\Dll\Project\Release No NetMon and AirPcap%VS_CONFIG_MODE%\Packet.dll"		.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\Dll\Project\x64\Release No NetMon and AirPcap%VS_CONFIG_MODE%\Packet.dll"	.\%DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\Dll\Project\Release No NetMon and AirPcap%VS_CONFIG_MODE%\Packet.dll"		.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\Dll\Project\x64\Release No NetMon and AirPcap%VS_CONFIG_MODE%\Packet.dll"	.\%DEPLOY_FOLDER_NAME%\x64\
 
 :: Copy NPFInstall.exe
-xcopy /Y	"..\packetWin7\NPFInstall\Release%VS_CONFIG_MODE%\NPFInstall.exe"							.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\NPFInstall\x64\Release%VS_CONFIG_MODE%\NPFInstall.exe"						.\%DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\NPFInstall\Release%VS_CONFIG_MODE%\NPFInstall.exe"							.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\NPFInstall\x64\Release%VS_CONFIG_MODE%\NPFInstall.exe"						.\%DEPLOY_FOLDER_NAME%\x64\
 
 :: Copy NPcapHelper.exe
-xcopy /Y	"..\packetWin7\Helper\release\NPcapHelper.exe"								.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\Helper\x64\release\NPcapHelper.exe"							.\%DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\Helper\release\NPcapHelper.exe"								.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\Helper\x64\release\NPcapHelper.exe"							.\%DEPLOY_FOLDER_NAME%\x64\
 
 :: Npcap uses the original WinPcap wpcap.dll with exactly the same code, we just changed the version number.
 :: Copy wpcap.dll
-xcopy /Y	"..\wpcap\PRJ\Release No AirPcap\x86\wpcap.dll"								.
-xcopy /Y	"..\wpcap\PRJ\Release No AirPcap\x64\wpcap.dll"								.\x64
+xcopy /Y	"..\wpcap\PRJ\Release No AirPcap\x86\wpcap.dll"								.\
+xcopy /Y	"..\wpcap\PRJ\Release No AirPcap\x64\wpcap.dll"								.\x64\
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: First need to add "signtool.exe" to PATH, then put the cert file (e.g. C:\xxx.pfx) to environment variable %NPF_CERT_PATH%,
--- installer/Deploy_WithTimeStamp.bat
+++ installer/Deploy_WithTimeStamp.bat
@@ -5,40 +5,40 @@ set VISTA_DEPLOY_FOLDER_NAME=vista
 set VS_CONFIG_MODE=
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Copy Npcap driver files
-xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"						.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"						.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"					.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"						.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"					.\%DEPLOY_FOLDER_NAME%\x64
-xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"					.\%DEPLOY_FOLDER_NAME%\x64
-xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"				.\%DEPLOY_FOLDER_NAME%\x64
-xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"					.\%DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"						.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"						.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"					.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"						.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"					.\%DEPLOY_FOLDER_NAME%\x64\
+xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"					.\%DEPLOY_FOLDER_NAME%\x64\
+xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"				.\%DEPLOY_FOLDER_NAME%\x64\
+xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"					.\%DEPLOY_FOLDER_NAME%\x64\
 
-xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"						.\%VISTA_DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"						.\%VISTA_DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"					.\%VISTA_DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"						.\%VISTA_DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"					.\%VISTA_DEPLOY_FOLDER_NAME%\x64
-xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"					.\%VISTA_DEPLOY_FOLDER_NAME%\x64
-xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"				.\%VISTA_DEPLOY_FOLDER_NAME%\x64
-xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"					.\%VISTA_DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"						.\%VISTA_DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"						.\%VISTA_DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"					.\%VISTA_DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"						.\%VISTA_DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.cat"					.\%VISTA_DEPLOY_FOLDER_NAME%\x64\
+xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.inf"					.\%VISTA_DEPLOY_FOLDER_NAME%\x64\
+xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%_wfp.inf"				.\%VISTA_DEPLOY_FOLDER_NAME%\x64\
+xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\npf Package\%DRIVER_NAME%.sys"					.\%VISTA_DEPLOY_FOLDER_NAME%\x64\
 
 :: Copy Packet.dll
-xcopy /Y	"..\packetWin7\Dll\Project\Release No NetMon and AirPcap%VS_CONFIG_MODE%\Packet.dll"		.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\Dll\Project\x64\Release No NetMon and AirPcap%VS_CONFIG_MODE%\Packet.dll"	.\%DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\Dll\Project\Release No NetMon and AirPcap%VS_CONFIG_MODE%\Packet.dll"		.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\Dll\Project\x64\Release No NetMon and AirPcap%VS_CONFIG_MODE%\Packet.dll"	.\%DEPLOY_FOLDER_NAME%\x64\
 
 :: Copy NPFInstall.exe
-xcopy /Y	"..\packetWin7\NPFInstall\Release%VS_CONFIG_MODE%\NPFInstall.exe"							.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\NPFInstall\x64\Release%VS_CONFIG_MODE%\NPFInstall.exe"						.\%DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\NPFInstall\Release%VS_CONFIG_MODE%\NPFInstall.exe"							.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\NPFInstall\x64\Release%VS_CONFIG_MODE%\NPFInstall.exe"						.\%DEPLOY_FOLDER_NAME%\x64\
 
 :: Copy NPcapHelper.exe
-xcopy /Y	"..\packetWin7\Helper\release\NPcapHelper.exe"								.\%DEPLOY_FOLDER_NAME%\x86
-xcopy /Y	"..\packetWin7\Helper\x64\release\NPcapHelper.exe"							.\%DEPLOY_FOLDER_NAME%\x64
+xcopy /Y	"..\packetWin7\Helper\release\NPcapHelper.exe"								.\%DEPLOY_FOLDER_NAME%\x86\
+xcopy /Y	"..\packetWin7\Helper\x64\release\NPcapHelper.exe"							.\%DEPLOY_FOLDER_NAME%\x64\
 
 :: Npcap uses the original WinPcap wpcap.dll with exactly the same code, we just changed the version number.
 :: Copy wpcap.dll
-xcopy /Y	"..\wpcap\PRJ\Release No AirPcap\x86\wpcap.dll"								.
-xcopy /Y	"..\wpcap\PRJ\Release No AirPcap\x64\wpcap.dll"								.\x64
+xcopy /Y	"..\wpcap\PRJ\Release No AirPcap\x86\wpcap.dll"								.\
+xcopy /Y	"..\wpcap\PRJ\Release No AirPcap\x64\wpcap.dll"								.\x64\
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: 1. Add "signtool.exe" to PATH
