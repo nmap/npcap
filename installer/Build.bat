bc621b21a774d444805c67b321d02fe0cfbf0729
hsluoyz <hsluoyz@qq.com>
2016-02-25 00:39:07 +0800

    Changed to build Packet project prior to wpcap, because wpcap's building depends on Packet's output.

--- installer/Build.bat
+++ installer/Build.bat
@@ -3,13 +3,13 @@
 :: Use VS2013's MSBuild to build wpcap.dll, Packet.dll, NPFInstall.exe and NPcapHelper.exe
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 
-msbuild "..\wpcap\PRJ\wpcap.sln" /t:Build /p:Configuration="Release - No AirPcap" /p:Platform="Win32"
-msbuild "..\wpcap\PRJ\wpcap.sln" /t:Build /p:Configuration="Release - No AirPcap" /p:Platform="x64"
-
 msbuild "..\packetWin7\Dll\Project\Packet.sln" /t:Build /p:Configuration="Release No NetMon and AirPcap" /p:Platform="Win32"
 msbuild "..\packetWin7\Dll\Project\Packet.sln" /t:Build /p:Configuration="Release No NetMon and AirPcap" /p:Platform="x64"
 msbuild "..\packetWin7\Dll\Project\Packet.sln" /t:Build /p:Configuration="Release No NetMon and AirPcap(WinPcap Mode)" /p:Platform="Win32"
 msbuild "..\packetWin7\Dll\Project\Packet.sln" /t:Build /p:Configuration="Release No NetMon and AirPcap(WinPcap Mode)" /p:Platform="x64"
+
+msbuild "..\wpcap\PRJ\wpcap.sln" /t:Build /p:Configuration="Release - No AirPcap" /p:Platform="Win32"
+msbuild "..\wpcap\PRJ\wpcap.sln" /t:Build /p:Configuration="Release - No AirPcap" /p:Platform="x64"
 
 msbuild "..\packetWin7\NPFInstall\NPFInstall.sln" /t:Build /p:Configuration="Release" /p:Platform="Win32"
 msbuild "..\packetWin7\NPFInstall\NPFInstall.sln" /t:Build /p:Configuration="Release" /p:Platform="x64"
