20db3d55e0a3595dca6d35a3a8e126c93c4bc6d5
hsluoyz <hsluoyz@qq.com>
2016-02-25 16:21:06 +0800

    Improved the macros in the installer deploy script.

--- installer/Gen_Installer_Only.bat
+++ installer/Gen_Installer_Only.bat
@@ -1,5 +1,7 @@
 
 set INSTALLER_NAME=npcap-nmap-0.05.exe
+
+set CERT_SIGN_TOOL="C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe"
 set CERT_MS_CROSS_CERT="C:\DigiCert High Assurance EV Root CA.crt"
 set CERT_HASH_VISTA="7e9978b1828447b97fbeba7c085dd1a217b07399"
 set CERT_HASH_WIN7_ABOVE="684d2e7df5b275515e703e6f42d962712b512da7"
@@ -11,7 +13,7 @@ set CERT_TIMESTAMP_SERVER=http://timestamp.digicert.com
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Sign the installer
-signtool sign /ac %CERT_MS_CROSS_CERT% /sha1 %CERT_HASH_VISTA% /fd sha1 /t %CERT_TIMESTAMP_SERVER% .\%INSTALLER_NAME%
-signtool sign /ac %CERT_MS_CROSS_CERT% /sha1 %CERT_HASH_WIN7_ABOVE% /as /fd sha256 /tr %CERT_TIMESTAMP_SERVER% /td sha256 .\%INSTALLER_NAME%
+%CERT_SIGN_TOOL% sign /ac %CERT_MS_CROSS_CERT% /sha1 %CERT_HASH_VISTA% /fd sha1 /t %CERT_TIMESTAMP_SERVER% .\%INSTALLER_NAME%
+%CERT_SIGN_TOOL% sign /ac %CERT_MS_CROSS_CERT% /sha1 %CERT_HASH_WIN7_ABOVE% /as /fd sha256 /tr %CERT_TIMESTAMP_SERVER% /td sha256 .\%INSTALLER_NAME%
 
 pause
