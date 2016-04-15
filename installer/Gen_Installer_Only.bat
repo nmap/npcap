23a52cd0c89cb68208234f820a1cb866ccec5550
hsluoyz <hsluoyz@qq.com>
2016-04-15 13:19:28 +0800

    Updated deploy scripts to use the new certificate to sign the files.

--- installer/Gen_Installer_Only.bat
+++ installer/Gen_Installer_Only.bat
@@ -3,8 +3,8 @@ set INSTALLER_NAME=npcap-nmap-0.06.exe
 
 set CERT_SIGN_TOOL="C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe"
 set CERT_MS_CROSS_CERT="C:\DigiCert High Assurance EV Root CA.crt"
-set CERT_HASH_VISTA="7e9978b1828447b97fbeba7c085dd1a217b07399"
-set CERT_HASH_WIN7_ABOVE="684d2e7df5b275515e703e6f42d962712b512da7"
+set CERT_HASH_VISTA="67cdca7703a01b25e6e0426072ec08b0046eb5f8"
+set CERT_HASH_WIN7_ABOVE="928101b5d0631c8e1ada651478e41afaac798b4c"
 set CERT_TIMESTAMP_SERVER=http://timestamp.digicert.com
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: First need to add "makensis.exe" to PATH
