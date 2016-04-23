805d832bc8ec968ea89a90eeb1fdabbf118f18d2
hsluoyz <hsluoyz@qq.com>
2016-04-23 23:12:25 +0800

    Rewrote the deploy symbols function via PowerShell.

--- installer/Deploy.ps1
+++ installer/Deploy.ps1
@@ -28,6 +28,12 @@ $installer_name = "npcap-nmap-0.06.exe"
 $nsis_compiler_tool = "C:\Program Files (x86)\NSIS\makensis.exe"
 
 ###########################################################
+# The variables about generating the symbols.
+$archive_7zip_tool = "C:\Program Files\7-Zip\7z.exe"
+$symbols_zip_name = $installer_name.Replace(".exe", "-DebugSymbols.zip")
+$symbols_folder = ".\npcap-DebugSymbols\"
+
+###########################################################
 # The npf/npcap driver.
 $driver_filename_array = "{0}.cat", "{0}.inf", "{0}_wfp.inf", "{0}.sys"
 $driver_init_from_path_array = 
@@ -188,14 +194,14 @@ function copy_and_sign($file_name, $from_path, $to_path)
 	if (!(Test-Path ($from_path + $file_name)))
 	{
 		Write-Host ("Error: source path not exist, path = " + $from_path + $file_name)
-		return
+		return 0
 	}
 	if (Test-Path ($to_path + $file_name))
 	{
 		if ((Get-Item ($from_path + $file_name)).LastWriteTime -le (Get-Item ($to_path + $file_name)).LastWriteTime)
 		{
 			Write-Host ("Info: source path is not modified, stop deploy it, source path = " + $from_path + $file_name)
-			return
+			return 0
 		}
 	}
 
@@ -205,7 +211,6 @@ function copy_and_sign($file_name, $from_path, $to_path)
 	}
 	Copy-Item ($from_path + $file_name) $to_path
 	Write-Host ("Info: copy source path to deployment folder, source path = " + $from_path + $file_name)
-	$has_file_updated = 1
 
 	if ($file_name -match ".sys" -or $file_name -match ".cat")
 	{
@@ -218,7 +223,7 @@ function copy_and_sign($file_name, $from_path, $to_path)
 			sign_driver_sha1 ($to_path + $file_name)
 		}
 	}
-	elseif ($file_name -match ".inf")
+	elseif ($file_name -match ".inf" -or $file_name -match ".pdb")
 	{
 
 	}
@@ -227,6 +232,8 @@ function copy_and_sign($file_name, $from_path, $to_path)
 		sign_file_sha1 ($to_path + $file_name)
 		sign_file_sha256 ($to_path + $file_name)
 	}
+
+	return 1
 }
 
 function sign_driver_sha1($file_path_name)
@@ -249,33 +256,67 @@ function sign_file_sha256($file_path_name)
 	&$cert_sign_tool "sign", "/ac", $cert_ms_cross_cert, "/sha1", $cert_hash_win7_above, "/as", "/fd", "sha256", "/tr", $cert_timestamp_server, "/td", "sha256", $file_path_name
 }
 
-function generate_installer($install_script, $installer_name, $has_check=1)
+function generate_installer($install_script, $installer_name)
 {
-	if ($has_check)
-	{
-		if ((Test-Path $installer_name) -and !$has_file_updated)
-		{
-			Write-Host ("Info: no deployment change, installer not generated.")
-			return
-		}
-	}
-
 	&$nsis_compiler_tool $install_script
 	sign_driver_sha1 $installer_name
 	sign_file_sha256 $installer_name
 }
 
-function do_deploy
+function generate_symbols($symbols_folder, $symbols_zip_name)
 {
+	&$archive_7zip_tool "a" $symbols_zip_name $symbols_folder
+}
+
+function do_deploy($installer_or_symbols = 1)
+{
+	if ($installer_or_symbols)
+	{
+		Write-Host ("Info: start deploy installer now.")
+	}
+	else
+	{
+		Write-Host ("Info: start deploy symbols now.")
+	}
+	
 	initialize_list ([ref]$file_name_array) ([ref]$from_path_array) ([ref]$to_path_array)
 
+	$has_file_updated = 0
 	for ($i = 0; $i -lt $file_name_array.Count; $i ++)
 	{
-		copy_and_sign $file_name_array[$i] $from_path_array[$i] $to_path_array[$i]
+		$res = copy_and_sign $file_name_array[$i] $from_path_array[$i] $to_path_array[$i]
+		$has_file_updated += $res
 		# echo ($file_name_array[$i] + ", " + $from_path_array[$i] + ", " + $to_path_array[$i])
 	}
+	Write-Host ("Info: Updated file count: " + $has_file_updated)
 
-	generate_installer (".\" + $install_script) (".\" + $installer_name)
+	if ($installer_or_symbols)
+	{
+		$install_script = ".\" + $install_script
+		$installer_name = ".\" + $installer_name
+		if ((Test-Path $installer_name) -and ($has_file_updated -eq 0))
+		{
+			Write-Host ("Info: no deployment change, installer not generated.")
+			return
+		}
+		else
+		{
+			generate_installer (".\" + $install_script) (".\" + $installer_name)
+		}
+	}
+	else
+	{
+		$symbols_zip_name = ".\" + $symbols_zip_name
+		if ((Test-Path $symbols_zip_name) -and ($has_file_updated -eq 0))
+		{
+			Write-Host ("Info: no deployment change, symbols not generated.")
+			return
+		}
+		else
+		{
+			generate_symbols ($symbols_folder + "*") $symbols_zip_name
+		}
+	}
 }
 
 if ($args.count -eq 0)
@@ -292,6 +333,24 @@ elseif ($args.count -eq 1)
 	{
 		$driver_init_from_path_array = $driver_init_from_path_array.replace("Release", "Debug")
 		do_deploy
+	}
+	elseif ($args[0] -eq "deploy-symbols")
+	{
+		$driver_filename_array = , "{0}.pdb"
+		$driver_init_from_path_array = $driver_init_from_path_array.replace("npf Package\", "")
+		$driver_init_to_path_array = $driver_init_to_path_array.replace(".\", $symbols_folder)
+
+		$init_to_path_array = $init_to_path_array.replace(".\", $symbols_folder)
+
+		$packet_filename = $packet_filename.replace(".dll", ".pdb")
+		$npfinstall_filename = $npfinstall_filename.replace(".exe", ".pdb")
+		$npcaphelper_filename = $npcaphelper_filename.replace(".exe", ".pdb")
+		$wlanhelper_filename = $wlanhelper_filename.replace(".exe", ".pdb")
+		$wpcap_filename = $wpcap_filename.replace(".dll", ".pdb")
+
+		$wpcap_init_to_path_array = $wpcap_init_to_path_array.replace(".\", $symbols_folder)
+
+		do_deploy 0
 	}
 	elseif ($args[0] -eq "installer")
 	{
--- installer/Deploy_Symbols.bat
+++ installer/Deploy_Symbols.bat
@@ -1,38 +1,11 @@
-
-set PDB_FOLDER_NAME=npcap-DebugSymbols
-set PDB_ZIP_NAME=npcap-nmap-0.06-DebugSymbols.zip
-set ARCHIVE_7ZIP_TOOL="C:\Program Files\7-Zip\7z.exe"
-
-set DRIVER_NAME=npcap
-set DEPLOY_FOLDER_NAME=win7_above
-set VISTA_DEPLOY_FOLDER_NAME=vista
-set VS_CONFIG_MODE=
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Copy PDB files for Npcap driver
-xcopy /Y	"..\packetWin7\npf\Win7 Release%VS_CONFIG_MODE%\%DRIVER_NAME%.pdb"						.\%PDB_FOLDER_NAME%\%DEPLOY_FOLDER_NAME%\x86\
-xcopy /Y	"..\packetWin7\npf\x64\Win7 Release%VS_CONFIG_MODE%\%DRIVER_NAME%.pdb"					.\%PDB_FOLDER_NAME%\%DEPLOY_FOLDER_NAME%\x64\
-xcopy /Y	"..\packetWin7\npf\Vista Release%VS_CONFIG_MODE%\%DRIVER_NAME%.pdb"						.\%PDB_FOLDER_NAME%\%VISTA_DEPLOY_FOLDER_NAME%\x86\
-xcopy /Y	"..\packetWin7\npf\x64\Vista Release%VS_CONFIG_MODE%\%DRIVER_NAME%.pdb"					.\%PDB_FOLDER_NAME%\%VISTA_DEPLOY_FOLDER_NAME%\x64\
-
-:: Copy PDB files for Packet.dll
-xcopy /Y	"..\packetWin7\Dll\Project\Release No NetMon and AirPcap%VS_CONFIG_MODE%\Packet.pdb"		.\%PDB_FOLDER_NAME%\%DEPLOY_FOLDER_NAME%\x86\
-xcopy /Y	"..\packetWin7\Dll\Project\x64\Release No NetMon and AirPcap%VS_CONFIG_MODE%\Packet.pdb"	.\%PDB_FOLDER_NAME%\%DEPLOY_FOLDER_NAME%\x64\
-
-:: Copy PDB files for NPFInstall.exe
-xcopy /Y	"..\packetWin7\NPFInstall\Release%VS_CONFIG_MODE%\NPFInstall.pdb"							.\%PDB_FOLDER_NAME%\%DEPLOY_FOLDER_NAME%\x86\
-xcopy /Y	"..\packetWin7\NPFInstall\x64\Release%VS_CONFIG_MODE%\NPFInstall.pdb"						.\%PDB_FOLDER_NAME%\%DEPLOY_FOLDER_NAME%\x64\
-
-:: Copy PDB files for NPcapHelper.exe
-xcopy /Y	"..\packetWin7\Helper\release\NPcapHelper.pdb"								.\%PDB_FOLDER_NAME%\%DEPLOY_FOLDER_NAME%\x86\
-xcopy /Y	"..\packetWin7\Helper\x64\release\NPcapHelper.pdb"							.\%PDB_FOLDER_NAME%\%DEPLOY_FOLDER_NAME%\x64\
-
-:: Npcap uses the original WinPcap wpcap.dll with exactly the same code, we just changed the version number.
-:: Copy PDB files for wpcap.dll
-xcopy /Y	"..\wpcap\PRJ\Release No AirPcap\x86\wpcap.pdb"								.\%PDB_FOLDER_NAME%\
-xcopy /Y	"..\wpcap\PRJ\Release No AirPcap\x64\wpcap.pdb"								.\%PDB_FOLDER_NAME%\x64\
-
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Generate the zip package for the PDB files using 7-Zip, make sure you installed 7-Zip first.
-%ARCHIVE_7ZIP_TOOL% a %PDB_ZIP_NAME% .\%PDB_FOLDER_NAME%\*
+@echo off
+set dir=%CD%
+set scriptPath=%dir%\Deploy.ps1
+for /f "tokens=*" %%a in ('powershell Get-ExecutionPolicy') do (
+set originPolicy=%%a
+)
+powershell Set-ExecutionPolicy 0
+powershell %scriptPath% deploy-symbols
+powershell Set-ExecutionPolicy %originPolicy%
 
 pause
