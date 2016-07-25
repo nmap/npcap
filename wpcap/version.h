5086d26c9846b6a8624ed675ed0f1444f988f075
Yang Luo <hsluoyz@qq.com>
2016-07-25 12:19:35 +0800

    [libpcap] Improved the libpcap version system.

---
+++ wpcap/version.h
@@ -0,0 +1,7 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+//
+
+#include "..\version.h"
+
+static const char pcap_version_string[] = "libpcap version " WINPCAP_LIBPCAP_VERSION;
