2d5d94f34424bbdc9bdc8ac9470c59696843ff9e
Yang Luo <hsluoyz@qq.com>
2016-08-13 17:54:05 +0800

    Libpcap version can be automatically generated based on the source.

--- wpcap/version.h
+++
@@ -1,7 +0,0 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-//
-
-#include "..\version.h"
-
-static const char pcap_version_string[] = "libpcap version " WINPCAP_LIBPCAP_VERSION;
