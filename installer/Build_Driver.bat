cde118c3d561dd43e75588a047e63f7913f1b477
Yang Luo <hsluoyz@qq.com>
2016-08-27 16:52:50 +0800

    Removed Vista in the build scripts.

--- installer/Build_Driver.bat
+++ installer/Build_Driver.bat
@@ -4,12 +4,8 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 
 :: "%28" is the escape for "(", "%29" is the escape for ")", and "%%" is the escape for "%" itself. Not using escape will cause target error of MSBuild.
-msbuild "..\packetWin7\npf\npf.sln" /t:Build /p:Configuration="Vista Release%%28WinPcap Mode%%29" /p:Platform="Win32"
-msbuild "..\packetWin7\npf\npf.sln" /t:Build /p:Configuration="Vista Release%%28WinPcap Mode%%29" /p:Platform="x64"
 msbuild "..\packetWin7\npf\npf.sln" /t:Build /p:Configuration="Win7 Release%%28WinPcap Mode%%29" /p:Platform="Win32"
 msbuild "..\packetWin7\npf\npf.sln" /t:Build /p:Configuration="Win7 Release%%28WinPcap Mode%%29" /p:Platform="x64"
-msbuild "..\packetWin7\npf\npf.sln" /t:Build /p:Configuration="Vista Release" /p:Platform="Win32"
-msbuild "..\packetWin7\npf\npf.sln" /t:Build /p:Configuration="Vista Release" /p:Platform="x64"
 msbuild "..\packetWin7\npf\npf.sln" /t:Build /p:Configuration="Win7 Release" /p:Platform="Win32"
 msbuild "..\packetWin7\npf\npf.sln" /t:Build /p:Configuration="Win7 Release" /p:Platform="x64"
 
