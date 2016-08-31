358e4f20e3161363bf8118599874e550f67e2c0f
Yang Luo <hsluoyz@qq.com>
2016-08-31 14:51:17 +0800

    Added Non-Admin running support for DiagReport.

--- installer/DiagReport.bat
+++ installer/DiagReport.bat
@@ -1,11 +1,28 @@
 @echo off
+
+net session >nul 2>&1
+if not "%errorLevel%" == "0" (
+  rem This tools must run with administrator permissions
+  rem It will popup the UAC dialog, please click [Yes] to continue.
+  echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+  echo UAC.ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\getadmin.vbs"
+  "%temp%\getadmin.vbs"
+  exit /b 2
+)
+
 set dir=.
 set scriptPath=%dir%\DiagReport.ps1
 for /f "tokens=*" %%a in ('powershell Get-ExecutionPolicy') do (
 set originPolicy=%%a
 )
 powershell Set-ExecutionPolicy 0
-powershell %scriptPath%
+
+rem this way only works for Administrator
+rem powershell %scriptPath%
+
+rem This call works also for normal users
+powershell -noprofile -NoExit -command "&{start-process powershell -ArgumentList '-NoExit -noprofile -file \"C:\Program Files\Npcap\DiagReport.ps1\"' -verb RunAs}"
+
 powershell Set-ExecutionPolicy %originPolicy%
 
 pause
