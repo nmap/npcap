6e94affe77e1d7738da01859dbb418e45d7fb6b4
Yang Luo <hsluoyz@qq.com>
2016-10-01 15:27:24 +0800

    Improved the strings to TCHAR in debug.h of NPFInstall.exe.

--- packetWin7/Dll/debug.h
+++ packetWin7/Dll/debug.h
@@ -38,10 +38,12 @@
 #include <stdio.h>
 #include <windows.h>
 
+#include <tchar.h>
+
 #pragma warning(push)
 #pragma warning(disable : 4127)
 
-static VOID OutputDebugStringVA(LPCSTR Format, ...)
+static VOID OutputDebugStringV(LPCTSTR Format, ...)
 {
 	FILE *f;											
 	SYSTEMTIME LocalTime;								
@@ -52,7 +54,7 @@ static VOID OutputDebugStringVA(LPCSTR Format, ...)
 
 	dwThreadId = GetCurrentThreadId();
 
-	va_start( Marker, Format );     /* Initialize variable arguments. */
+	va_start(Marker, Format); /* Initialize variable arguments. */
 														
 	GetLocalTime(&LocalTime);							
 														
@@ -60,9 +62,9 @@ static VOID OutputDebugStringVA(LPCSTR Format, ...)
 	{
 
 #ifdef _CONSOLE
-		if (fopen_s(&f, "C:\\Program Files\\Npcap\\NPFInstall.log", "a") == 0)
+		if (_wfopen_s(&f, _T("C:\\Program Files\\Npcap\\NPFInstall.log"), _T("a")) == 0)
 #else
-		if (fopen_s(&f, "C:\\Program Files\\Npcap\\Packet.log", "a") == 0)
+		if (_wfopen_s(&f, _T("C:\\Program Files\\Npcap\\Packet.log"), _T("a")) == 0)
 #endif
 			break;
 
@@ -77,7 +79,7 @@ static VOID OutputDebugStringVA(LPCSTR Format, ...)
 	}
 	while(1);
 
-	fprintf(f, "[%.08X] %.04u-%.02u-%.02u %.02u:%02u:%02u ",
+	_ftprintf(f, _T("[%.08X] %.04u-%.02u-%.02u %.02u:%02u:%02u "),
 			dwThreadId,
 			LocalTime.wYear,							
 			LocalTime.wMonth,							
@@ -85,61 +87,9 @@ static VOID OutputDebugStringVA(LPCSTR Format, ...)
 			LocalTime.wHour,							
 			LocalTime.wMinute,							
 			LocalTime.wSecond);										
-	vfprintf(f, Format, Marker);
+	_vftprintf(f, Format, Marker);
 	
 	fclose(f);											
-
-
-	SetLastError(dwLastError);
-}
-
-static VOID OutputDebugStringVW(LPCWSTR Format, ...)
-{
-	FILE *f;											
-	SYSTEMTIME LocalTime;								
-	va_list Marker;
-	DWORD dwThreadId;
-	int loops = 0;
-	DWORD dwLastError = GetLastError();
-
-	dwThreadId = GetCurrentThreadId();
-
-	va_start( Marker, Format );     /* Initialize variable arguments. */
-
-	GetLocalTime(&LocalTime);							
-
-	do
-	{
-
-#ifdef _CONSOLE
-		if (_wfopen_s(&f, L"C:\\Program Files\\Npcap\\NPFInstall.log", L"a") == 0)
-#else
-		if (_wfopen_s(&f, L"C:\\Program Files\\Npcap\\Packet.log", L"a") == 0)
-#endif
-			break;
-
-		Sleep(0);
-		loops++;
-
-		if (loops > 10)
-		{
-			SetLastError(dwLastError);
-			return;
-		}
-	}
-	while(1);
-
-	fwprintf(f, L"[%.08X] %.04u-%.02u-%.02u %.02u:%02u:%02u ",
-		dwThreadId,
-		LocalTime.wYear,							
-		LocalTime.wMonth,							
-		LocalTime.wDay,								
-		LocalTime.wHour,
-		LocalTime.wMinute,
-		LocalTime.wSecond);
-	vfwprintf(f, Format, Marker);
-
-	fclose(f);
 
 
 	SetLastError(dwLastError);
@@ -151,34 +101,17 @@ static VOID OutputDebugStringVW(LPCWSTR Format, ...)
 
 #include <strsafe.h>
 
-static VOID OutputDebugStringVA(LPCSTR Format, ...)
+static VOID OutputDebugStringV(LPCTSTR Format, ...)
 {
 	va_list Marker;
-	CHAR string[1024];
+	TCHAR string[1024];
 	DWORD dwLastError = GetLastError();
 
-	va_start( Marker, Format );     /* Initialize variable arguments. */
+	va_start(Marker, Format); /* Initialize variable arguments. */
 
-	StringCchVPrintfA(string, sizeof(string), Format, Marker);
+	StringCchVPrintf(string, sizeof(string), Format, Marker);
 
-	OutputDebugStringA(string);
-
-	va_end(Marker);
-
-	SetLastError(dwLastError);
-}
-
-static VOID OutputDebugStringVW(LPCWSTR Format, ...)
-{
-	va_list Marker;
-	wchar_t string[1024];
-	DWORD dwLastError = GetLastError();
-
-	va_start( Marker, Format );     /* Initialize variable arguments. */
-
-	StringCchVPrintfW(string, sizeof(string), Format, Marker);
-
-	OutputDebugStringW(string);
+	OutputDebugString(string);
 
 	va_end(Marker);
 
@@ -195,17 +128,17 @@ static VOID OutputDebugStringVW(LPCWSTR Format, ...)
 #define TRACE_PRINT_DLLMAIN(_x)							//we cannot use the _DEBUG_TO_FILE stuff from DllMain!!
 #endif
 
-#define TRACE_ENTER()									OutputDebugStringVA ("--> " __FUNCTION__ "\n")
-#define TRACE_EXIT()									OutputDebugStringVA ("<-- " __FUNCTION__ "\n")
-#define TRACE_PRINT(_x)									OutputDebugStringVA ("    " _x "\n")
-#define TRACE_PRINT1(_x, _y)							OutputDebugStringVA("    " _x "\n", _y)
-#define TRACE_PRINT2(_x, _p1, _p2)						OutputDebugStringVA("    " _x "\n", _p1, _p2)
-#define TRACE_PRINT3(_x, _p1, _p2, _p3)					OutputDebugStringVA("    " _x "\n", _p1, _p2, _p3)
-#define TRACE_PRINT4(_x, _p1, _p2, _p3, _p4)			OutputDebugStringVA("    " _x "\n", _p1, _p2, _p3, _p4)
-#define TRACE_PRINT5(_x, _p1, _p2, _p3, _p4, _p5)		OutputDebugStringVA("    " _x "\n", _p1, _p2, _p3, _p4, _p5)
-#define TRACE_PRINT6(_x, _p1, _p2, _p3, _p4, _p5, _p6)	OutputDebugStringVA("    " _x "\n", _p1, _p2, _p3, _p4, _p5, _p6 )
+#define TRACE_ENTER()									OutputDebugStringV(_T("--> ") _T(__FUNCTION__) _T("\n"))
+#define TRACE_EXIT()									OutputDebugStringV(_T("<-- ") _T(__FUNCTION__) _T("\n"))
+#define TRACE_PRINT(_x)									OutputDebugStringV(_T("    ") _T(_x) _T("\n"))
+#define TRACE_PRINT1(_x, _p1)							OutputDebugStringV(_T("    ") _T(_x) _T("\n"), _p1)
+#define TRACE_PRINT2(_x, _p1, _p2)						OutputDebugStringV(_T("    ") _T(_x) _T("\n"), _p1, _p2)
+#define TRACE_PRINT3(_x, _p1, _p2, _p3)					OutputDebugStringV(_T("    ") _T(_x) _T("\n"), _p1, _p2, _p3)
+#define TRACE_PRINT4(_x, _p1, _p2, _p3, _p4)			OutputDebugStringV(_T("    ") _T(_x) _T("\n"), _p1, _p2, _p3, _p4)
+#define TRACE_PRINT5(_x, _p1, _p2, _p3, _p4, _p5)		OutputDebugStringV(_T("    ") _T(_x) _T("\n"), _p1, _p2, _p3, _p4, _p5)
+#define TRACE_PRINT6(_x, _p1, _p2, _p3, _p4, _p5, _p6)	OutputDebugStringV(_T("    ") _T(_x) _T("\n"), _p1, _p2, _p3, _p4, _p5, _p6)
 
-#define TRACE_PRINT_WIDECHAR(_x)						OutputDebugStringVW (L"    %ws\n", _x)
+#define TRACE_PRINT_WIDECHAR(_x)						OutputDebugStringV(_T("    %ws\n"), _x)
 
 static __forceinline void TRACE_PRINT_OS_INFO()
 {
@@ -220,79 +153,81 @@ static __forceinline void TRACE_PRINT_OS_INFO()
 	TRACE_PRINT("********************* OS info.*********************");
 	buffer[size-1] = 0;
 	size = sizeof(buffer);
-	if(	RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", 0, KEY_READ, &hKey) == ERROR_SUCCESS)
+	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, _T("SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment"), 0, KEY_READ, &hKey) == ERROR_SUCCESS)
 	{
-		if (RegQueryValueExA(hKey, "PROCESSOR_ARCHITECTURE", 0, &type, (LPBYTE)buffer, &size) == ERROR_SUCCESS && type == REG_SZ)
+		if (RegQueryValueEx(hKey, _T("PROCESSOR_ARCHITECTURE"), 0, &type, (LPBYTE)buffer, &size) == ERROR_SUCCESS && type == REG_SZ)
 		{
-			OutputDebugStringVA("Architecture = %s\n", buffer);
+			OutputDebugStringV(_T("Architecture = %s\n"), buffer);
 		}
 		else
 		{
-			OutputDebugStringVA("Architecture = <UNKNOWN>\n");
+			OutputDebugStringV(_T("Architecture = <UNKNOWN>\n"));
 		}
 		
 		RegCloseKey(hKey);
 	}
 	else
 	{
-		OutputDebugStringVA("Architecture = <UNKNOWN>\n");
+		OutputDebugStringV(_T("Architecture = <UNKNOWN>\n"));
 	}
 
 	buffer[size-1] = 0;
 	size = sizeof(buffer);
 
-	if(	RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", 0, KEY_READ, &hKey) == ERROR_SUCCESS)
+	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, _T("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion"), 0, KEY_READ, &hKey) == ERROR_SUCCESS)
 	{
-		if (RegQueryValueExA(hKey, "CurrentVersion", 0, &type,  (LPBYTE)buffer, &size) == ERROR_SUCCESS && type == REG_SZ)
+		if (RegQueryValueEx(hKey, _T("CurrentVersion"), 0, &type,  (LPBYTE)buffer, &size) == ERROR_SUCCESS && type == REG_SZ)
 		{
-			OutputDebugStringVA("Windows version = %s\n", buffer);
+			OutputDebugStringV(_T("Windows version = %s\n"), buffer);
 		}
 		else
 		{
-			OutputDebugStringVA("Windows version = <UNKNOWN>\n");
+			OutputDebugStringV(_T("Windows version = <UNKNOWN>\n"));
 		}
 		
 		RegCloseKey(hKey);
 	}
 	else
 	{
-		OutputDebugStringVA("Windows version = <UNKNOWN>\n");
+		OutputDebugStringV(_T("Windows version = <UNKNOWN>\n"));
 	}
 
 	buffer[size-1] = 0;
 	size = sizeof(buffer);
-	if(	RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", 0, KEY_READ, &hKey) == ERROR_SUCCESS)
+	if(	RegOpenKeyEx(HKEY_LOCAL_MACHINE, _T("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion"), 0, KEY_READ, &hKey) == ERROR_SUCCESS)
 	{
-		if (RegQueryValueExA(hKey, "CurrentType", 0, &type,  (LPBYTE)buffer, &size) == ERROR_SUCCESS && type == REG_SZ)
+		if (RegQueryValueEx(hKey, _T("CurrentType"), 0, &type,  (LPBYTE)buffer, &size) == ERROR_SUCCESS && type == REG_SZ)
 		{
-			OutputDebugStringVA("Windows CurrentType = %s\n", buffer);
+			OutputDebugStringV(_T("Windows CurrentType = %s\n"), buffer);
 		}
 		else
 		{
-			OutputDebugStringVA("Windows CurrentType = <UNKNOWN>\n");
+			OutputDebugStringV(_T("Windows CurrentType = <UNKNOWN>\n"));
 		}
 		
 		RegCloseKey(hKey);
 	}
 	else
 	{
-		OutputDebugStringVA("Windows CurrentType = <UNKNOWN>\n");
+		OutputDebugStringV(_T("Windows CurrentType = <UNKNOWN>\n"));
 	}
 
-	OutputDebugStringVA("*************************************************** \n");
+	OutputDebugStringV(_T("*************************************************** \n"));
 
 	SetLastError(dwLastError);
 }
 #else
 
-#define TRACE_ENTER(_x)
+#define TRACE_ENTER()
 #define TRACE_PRINT_DLLMAIN(_x)
-#define TRACE_EXIT(_x) 
+#define TRACE_EXIT()
 #define TRACE_PRINT(_x)
-#define TRACE_PRINT1(_x, _y)
+#define TRACE_PRINT1(_x, _p1)
 #define TRACE_PRINT2(_x, _p1, _p2)
-#define TRACE_PRINT4(_x, _p1, _p2, _p3, _p4) 
-#define TRACE_PRINT6(_x, _p1, _p2, _p3, _p4, _p5, _p6) 
+#define TRACE_PRINT3(_x, _p1, _p2, _p3)
+#define TRACE_PRINT4(_x, _p1, _p2, _p3, _p4)
+#define TRACE_PRINT5(_x, _p1, _p2, _p3, _p4, _p5)
+#define TRACE_PRINT6(_x, _p1, _p2, _p3, _p4, _p5, _p6)
 #define TRACE_PRINT_WIDECHAR(_x)
 #define TRACE_PRINT_OS_INFO()
 
