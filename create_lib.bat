0499dcda18615e46546b4660afd1ee16967aa947
Yang Luo <hsluoyz@qq.com>
2016-10-06 16:57:28 +0800

    Fixed the path of latest libpcap's wpcap.lib in SDK.

--- create_lib.bat
+++ create_lib.bat
@@ -9,8 +9,8 @@ mkdir %WPDPACKDESTDIR% 		>nul 2>nul
 mkdir %WPDPACKDESTDIR%\Lib 	>nul 2>nul
 mkdir %WPDPACKDESTDIR%\Lib\x64	>nul 2>nul
 
-xcopy /v /Y "%WINPCAPSOURCEDIR%\wpcap\PRJ\Release No AirPcap\x86\wpcap.lib"				%WPDPACKDESTDIR%\Lib\ >nul
-xcopy /v /Y "%WINPCAPSOURCEDIR%\wpcap\PRJ\Release No AirPcap\x64\wpcap.lib"				%WPDPACKDESTDIR%\Lib\x64 >nul
+xcopy /v /Y "%WINPCAPSOURCEDIR%\wpcap\libpcap\Win32\Prj\Release\wpcap.lib"				%WPDPACKDESTDIR%\Lib\ >nul
+xcopy /v /Y "%WINPCAPSOURCEDIR%\wpcap\libpcap\Win32\Prj\x64\Release\wpcap.lib"				%WPDPACKDESTDIR%\Lib\x64 >nul
 xcopy /v /Y "%WINPCAPSOURCEDIR%\packetWin7\Dll\Project\Release No NetMon and AirPcap\packet.lib"	 	%WPDPACKDESTDIR%\Lib\ >nul
 xcopy /v /Y "%WINPCAPSOURCEDIR%\packetWin7\Dll\Project\x64\Release No NetMon and AirPcap\packet.lib"	 	%WPDPACKDESTDIR%\Lib\x64 >nul
 
