659c02d59d8b878b7582f2a9a793688605cb0628
Yang Luo <hsluoyz@qq.com>
2016-12-03 14:56:16 +0800

    Replaced PSAPI's EnumProcesses() with ToolHelp32 API.

--- packetWin7/NPFInstall/NPFInstall/ProcessUtil.cpp
+++ packetWin7/NPFInstall/NPFInstall/ProcessUtil.cpp
@@ -14,6 +14,7 @@ Get processes which are using Npcap DLLs.
 
 #include <windows.h>
 #include <psapi.h>
+#include <TlHelp32.h>
 #include <tchar.h>
 
 #include <algorithm>
@@ -24,24 +25,6 @@ using namespace std;
 #include "ProcessUtil.h"
 #include "debug.h"
 
-
-tstring getProcessName(HANDLE hProcess)
-{
-	tstring strResult;
-	TCHAR pFullPath[_MAX_PATH] = _T("");
-	TCHAR pDrive[_MAX_DRIVE];
-	TCHAR pDir[_MAX_DIR];
-	TCHAR pFilename[_MAX_FNAME];
-	TCHAR pExt[_MAX_EXT];
-
-	GetModuleFileNameEx(hProcess, NULL, pFullPath, MAX_PATH);
-
-	_tsplitpath_s(pFullPath, pDrive, _MAX_DRIVE, pDir, _MAX_DIR, pFilename, _MAX_FNAME, pExt, _MAX_EXT);
-
-	strResult = pFilename;
-	strResult += pExt;
-	return strResult;
-}
 
 BOOL checkModulePathName(tstring strModulePathName)
 {
@@ -65,9 +48,8 @@ BOOL checkModulePathName(tstring strModulePathName)
 	}
 }
 
-tstring enumDLLs(DWORD dwProcessID)
+BOOL enumDLLs(tstring strProcessName, DWORD dwProcessID)
 {
-	tstring strProcessName = _T("");
 	HMODULE hArrModules[1024];
 	HANDLE hProcess;
 	DWORD cbNeeded;
@@ -80,8 +62,9 @@ tstring enumDLLs(DWORD dwProcessID)
 	hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, dwProcessID);
 	if (!hProcess)
 	{
-		TRACE_PRINT2("enumDLLs::OpenProcess: error, errCode = 0x%08x, dwProcessID = %d.", GetLastError(), dwProcessID);
-		return strProcessName;
+		TRACE_PRINT3("enumDLLs::OpenProcess: error, errCode = 0x%08x, strProcessName = %s, dwProcessID = %d.", GetLastError(), strProcessName.c_str(), dwProcessID);
+		_tprintf(_T("enumDLLs::OpenProcess: error, errCode = 0x % 08x, strProcessName = %s, dwProcessID = %d.\n"), GetLastError(), strProcessName.c_str(), dwProcessID);
+		return FALSE;
 	}
 
 	if (EnumProcessModules(hProcess, hArrModules, sizeof(hArrModules), &cbNeeded))
@@ -93,70 +76,60 @@ tstring enumDLLs(DWORD dwProcessID)
 			// Get the full path to the module's file.
 			if (GetModuleFileNameEx(hProcess, hArrModules[i], szModName, sizeof(szModName)))
 			{
-				// Print the module name and handle value.
-				// _tprintf(_T("\t%s (0x%08p)\n"), szModName, hArrModules[i]);
-
 				tstring strModulePathName = szModName;
 				transform(strModulePathName.begin(), strModulePathName.end(), strModulePathName.begin(), ::tolower);
-				// Print the module name and handle value.
-				// _tprintf(_T("\t%s (0x%08p)\n"), strModulePathName.c_str(), hArrModules[i]);
 
 				if (checkModulePathName(strModulePathName))
 				{
-					strProcessName = getProcessName(hProcess);
 					TRACE_PRINT2("enumDLLs: succeed, strProcessName = %s, strModulePathName = %s.", strProcessName.c_str(), strModulePathName.c_str());
-					// _tprintf(_T("\t%s, %s\n"), strModulePathName.c_str(), strProcessName.c_str());
+					// _tprintf(_T("enumDLLs: succeed, strProcessName = %s, strModulePathName = %s.\n"), strProcessName.c_str(), strModulePathName.c_str());
+					return TRUE;
 				}
-// 				else
-// 				{
-// 					strProcessName = getProcessName(hProcess);
-// 					TRACE_PRINT1("enumDLLs: succeed, strProcessName = %s, strModulePathName = <NULL>.", strProcessName.c_str());
-// 				}
+				else
+				{
+					TRACE_PRINT1("enumDLLs: succeed, strProcessName = %s, strModulePathName = <NULL>.", strProcessName.c_str());
+					// _tprintf(_T("enumDLLs: succeed, strProcessName = %s, strModulePathName = <NULL>.\n"), strProcessName.c_str());
+				}
 			}
 		}
 	}
 	else
 	{
 		TRACE_PRINT1("EnumProcessModules: error, errCode = 0x%08x.", GetLastError());
-		return strProcessName;
+		return FALSE;
 	}
 
 	CloseHandle(hProcess);
 
-	return strProcessName;
+	return FALSE;
 }
 
 vector<tstring> enumProcesses()
 {
-	TRACE_ENTER();
-
 	vector<tstring> strArrProcessNames;
-	DWORD dwArrProcessIDs[2048];
-	DWORD cbNeeded;
-	DWORD cProcesses;
 
-	// Get the list of process identifiers.
-	if (!EnumProcesses(dwArrProcessIDs, sizeof(dwArrProcessIDs), &cbNeeded))
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	if (hSnapshot)
 	{
-		TRACE_PRINT1("EnumProcesses: error, errCode = 0x%08x.", GetLastError());
-		TRACE_EXIT();
-		return strArrProcessNames;
-	}
-
-	// Calculate how many process identifiers were returned.
-	cProcesses = cbNeeded / sizeof(DWORD);
-
-	// Enumerate the DLL modules for each process.
-	for (DWORD i = 0; i < cProcesses; i ++)
-	{
-		tstring strProcessName = enumDLLs(dwArrProcessIDs[i]);
-		if (strProcessName != _T(""))
+		PROCESSENTRY32 PEInfo;
+		PEInfo.dwSize = sizeof(PEInfo);
+		BOOL bHasNextProcess = Process32First(hSnapshot, &PEInfo);
+		bool found = false;
+		while (bHasNextProcess)
 		{
-			strArrProcessNames.push_back(strProcessName);
+			bHasNextProcess = Process32Next(hSnapshot, &PEInfo);
+			tstring strProcessName = PEInfo.szExeFile;
+			// _tprintf(_T("szExeFile = %s, th32ProcessID = %d\n"), PEInfo.szExeFile, PEInfo.th32ProcessID);
+			BOOL bHasNpcapDLL = enumDLLs(strProcessName, PEInfo.th32ProcessID);
+			if (bHasNpcapDLL)
+			{
+				strArrProcessNames.push_back(strProcessName);
+			}
 		}
+
+		CloseHandle(hSnapshot);
 	}
 
-	TRACE_EXIT();
 	return strArrProcessNames;
 }
 
