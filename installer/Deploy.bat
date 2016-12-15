fdd94374dc48bce58cc440adadc37f2406ff5533
Daniel Miller <dmiller@nmap.org>
2016-12-15 09:44:00 -0600

    Only set execution policy for current user

--- installer/Deploy.bat
+++ installer/Deploy.bat
@@ -4,8 +4,8 @@ set scriptPath=%dir%\Deploy.ps1
 for /f "tokens=*" %%a in ('powershell Get-ExecutionPolicy') do (
 set originPolicy=%%a
 )
-powershell Set-ExecutionPolicy 0
+powershell Set-ExecutionPolicy -scope currentuser 0
 powershell %scriptPath%
-powershell Set-ExecutionPolicy %originPolicy%
+powershell Set-ExecutionPolicy -scope currentuser %originPolicy%
 
 pause
