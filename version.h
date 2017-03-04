e37deb55fa5aef036695cc1ce1e1b2aa643217a7
Daniel Miller <dmiller@nmap.org>
2017-03-04 15:05:05 -0600

    Bump version and changelog for 0.83

--- version.h
+++ version.h
@@ -87,11 +87,11 @@
 #define /*
  !define /**/ WINPCAP_MINOR				0
 #define /*
- !define /**/ WINPCAP_REV				82
+ !define /**/ WINPCAP_REV				83
 #define /*
- !define /**/ WINPCAP_BUILD				223
+ !define /**/ WINPCAP_BUILD				304
 #define /*
- !define /**/ WINPCAP_VER_STRING		"0.82"
+ !define /**/ WINPCAP_VER_STRING		"0.83"
 
 #define WINPCAP_WPCAP_STRING_VERSION WINPCAP_VER_STRING
 
