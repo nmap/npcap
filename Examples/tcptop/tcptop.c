efd207c916c41996cc07b59c21645cea28a78d79
Daniel Miller <dmiller@nmap.org>
2018-08-17 15:03:12 -0500

    Update tcptop example project for VS 2015

--- Examples/tcptop/tcptop.c
+++ Examples/tcptop/tcptop.c
@@ -36,6 +36,24 @@
 
 #include <pcap.h>
 
+#include <tchar.h>
+BOOL LoadNpcapDlls()
+{
+    _TCHAR npcap_dir[512];
+    UINT len;
+    len = GetSystemDirectory(npcap_dir, 480);
+    if (!len) {
+        fprintf(stderr, "Error in GetSystemDirectory: %x", GetLastError());
+        return FALSE;
+    }
+    _tcscat_s(npcap_dir, 512, _T("\\Npcap"));
+    if (SetDllDirectory(npcap_dir) == 0) {
+        fprintf(stderr, "Error in SetDllDirectory: %x", GetLastError());
+        return FALSE;
+    }
+    return TRUE;
+}
+
 void usage();
 
 void dispatcher_handler(u_char *, const struct pcap_pkthdr *, const u_char *);
@@ -49,6 +67,13 @@ struct timeval st_ts;
 u_int netmask;
 struct bpf_program fcode;
   
+    /* Load Npcap and its functions. */
+    if (!LoadNpcapDlls())
+    {
+        fprintf(stderr, "Couldn't load Npcap\n");
+        exit(1);
+    }
+
 	/* Check the validity of the command line */
 	if (argc != 2)
 	{
