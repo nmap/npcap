2a1edb17f4512ca26a02cb008d419e8cea7cd114
Daniel Miller <dmiller@nmap.org>
2018-09-25 23:35:23 -0500

    Parameterize and note some stuff in sdk build script

--- build_sdk.bat
+++ build_sdk.bat
@@ -1,15 +1,19 @@
 @echo off
+set SDKFILENAME=npcap-sdk-0.1.zip
 
 if "%2"== "" ( rd /s/q ./npcap-sdk 2>nul >nul) else ( rd /s /q "%2" 2>nul >nul)
 
+rem Requires Cygwin to provide make.exe
 call create_include.bat %1 %2
 
 call create_lib.bat %1 %2
 
 call create_examples.bat %1 %2
 
+rem Requires xsltproc and Docbook XSL stylesheets
 call create_docs.bat %1 %2
 
-"C:\Program Files\7-Zip\7z.exe" a installer\npcap-sdk-0.07.zip .\npcap-sdk
+del %SDKFILENAME%
+"C:\Program Files\7-Zip\7z.exe" a %SDKFILENAME% .\npcap-sdk
 PAUSE
 
