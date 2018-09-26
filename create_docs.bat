f2de5adf427dbfcc6f19e8718ab70395aa736cef
Daniel Miller <dmiller@nmap.org>
2018-09-25 23:34:56 -0500

    Use docbook XSL instead of doxygen to generate SDK docs

--- create_docs.bat
+++ create_docs.bat
@@ -1,31 +1,21 @@
 @echo off
 
-IF "%2"=="" (set WPDPACKDESTDIR=.\npcap-sdk\) ELSE (set WPDPACKDESTDIR=%2)
+IF "%2"=="" (set WPDPACKDESTDIR=.\npcap-sdk) ELSE (set WPDPACKDESTDIR=%2)
 
 IF ""=="%1" (set WINPCAPSOURCEDIR=.\) ELSE (set WINPCAPSOURCEDIR=%1) 
+
+set DOCBOOKXSL=C:\xslt\docbook-xsl-1.79.2
+set XSLTPROC=C:\xslt\bin\xsltproc.exe
 
 echo Creating \docs folder
 mkdir %WPDPACKDESTDIR% >nul 2>nul
 mkdir %WPDPACKDESTDIR%\docs >nul 2>nul
-mkdir %WPDPACKDESTDIR%\docs\html >nul 2>nul
-
-pushd %WINPCAPSOURCEDIR%\dox\prj
 
 echo - Deleting existing WinPcap documentation
-del /q docs\*.* 2> nul > nul
+del /q %WPDPACKDESTDIR%\docs\*.* 2> nul > nul
 echo - Creating new documentation
-doxygen winpcap_noc.dox >nul
-echo - Copying all gif files
-xcopy ..\pics\*.gif docs\. /v /y /q >nul
-xcopy ..\*.gif docs\. /v /y /q >nul
+%XSLTPROC% --path %DOCBOOKXSL% --nonet --stringparam media.type html --stringparam base.dir %WPDPACKDESTDIR%/docs/ --stringparam use.id.as.filename 1 %DOCBOOKXSL%\html\chunk.xsl %WINPCAPSOURCEDIR%\docs\npcap-guide-wrapper.xml
 
-popd
-
-xcopy /v /Y %WINPCAPSOURCEDIR%\dox\WinPcap_docs.html	%WPDPACKDESTDIR%\docs\ 		>nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\dox\prj\docs\*.*		%WPDPACKDESTDIR%\docs\html\	>nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\dox\*.gif		%WPDPACKDESTDIR%\docs\html\	>nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\dox\pics\*.gif		%WPDPACKDESTDIR%\docs\html\	>nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\docs\npcap-guide-wrapper.html		%WPDPACKDESTDIR%\docs\	>nul
 echo Folder \docs created successfully
 set WPDPACKDESTDIR=
 set WINPCAPSOURCEDIR=
