212eae2f138baa80f8ed64e950437d2c18b4acf5
Daniel Miller <dmiller@nmap.org>
2019-01-06 17:27:51 -0600

    Standardize on NDIS 6.1 (Vista SP1) and keep defs in vcxproj so they can be updated or reconfigured

--- packetWin7/npf/npf/stdafx.h
+++ packetWin7/npf/npf/stdafx.h
@@ -53,28 +53,6 @@
 #endif
 #define WIN_NT_DRIVER
 
-#define NTDDI_WIN7 0x06010000
-#if (NTDDI_VERSION >= NTDDI_WIN7)
-#define NDIS_SUPPORT_NDIS620 1
-#define NDIS620 1 // NDIS 6.2 version (for Windows 7 and later)
-#else
-#define NDIS_SUPPORT_NDIS6 1
-#define NDIS60 1 // NDIS 6.0 version (for Windows Vista and later)
-#endif
-
-// Specify which version of the NDIS contract you will use here.
-// In many cases, 6.0 is the best choice.  You only need to select a later
-// version if you need a feature that is not available in 6.0.
-//
-// Legal values include:
-//    6.0  Available starting with Windows Vista RTM
-//    6.1  Available starting with Windows Vista SP1 / Windows Server 2008
-//    6.20 Available starting with Windows 7 / Windows Server 2008 R2
-//    6.30 Available starting with Windows 8 / Windows Server "8"
-// Or, just use NDIS_FILTER_MAJOR_VERSION / NDIS_FILTER_MINOR_VERSION
-// to pick up whatever version is defined by your build system
-// (for example, "-DNDIS630").
-
 #define NPF_ALLOC_TAG '1234'
 #define NPF_REQUEST_ID '5678'
 
