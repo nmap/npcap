a5ebb08c2f4d445e1abbc5ae6060ded9230da819
Daniel Miller <dmiller@nmap.org>
2019-03-24 16:02:15 -0500

    Update libpcap to 1.9.0, using CMake to build. See nmap/nmap#1506

--- installer/Build.bat
+++ installer/Build.bat
@@ -11,5 +11,8 @@ msbuild "%NPCAPDIR%\packetWin7\vs14\npcap.sln" /m /t:Build /p:Configuration="%MO
 msbuild "%NPCAPDIR%\packetWin7\vs14\npcap.sln" /m /t:Build /p:Configuration="%MODE% No NetMon and AirPcap%%28WinPcap Mode%%29" /p:Platform="x86"
 msbuild "%NPCAPDIR%\packetWin7\vs14\npcap.sln" /m /t:Build /p:Configuration="%MODE% No NetMon and AirPcap%%28WinPcap Mode%%29" /p:Platform="x64"
 
+msbuild "%NPCAPDIR%\wpcap\build-win32\wpcap.vcxproj" /m /t:Build /p:Configuration="%MODE%" /p:Platform="Win32"
+msbuild "%NPCAPDIR%\wpcap\build-x64\wpcap.vcxproj" /m /t:Build /p:Configuration="%MODE%" /p:Platform="x64"
+
 pause
 
---
+++ wpcap/wpcap-cmake.bat
@@ -0,0 +1,13 @@
+set CMAKE="C:\Program Files\CMake\bin\cmake.exe"
+set GENERATOR=Visual Studio 14 2015
+set NPCAP_SDK=..\..\npcap-sdk-1.0\
+
+mkdir build-win32
+cd build-win32
+%CMAKE% -DPACKET_DLL_DIR=..\%NPCAP_SDK% -DLIBRARY_NAME=wpcap -G "%GENERATOR%" ..\libpcap\
+cd ..
+
+mkdir build-x64
+cd build-x64
+%CMAKE% -DPACKET_DLL_DIR=..\%NPCAP_SDK% -DLIBRARY_NAME=wpcap -G "%GENERATOR% Win64" ..\libpcap\
+cd ..
