212db04f543de23e2b45a31fa5bbc90522530d3f
Daniel Miller <dmiller@nmap.org>
2020-03-15 15:31:47 -0500

    Remove TIMESTAMPMODE_RDTSC. See nmap/nmap#1829.
    Use of RDTSC instruction is discouraged by Microsoft
    (https://docs.microsoft.com/en-us/windows/win32/sysinfo/acquiring-high-resolution-time-stamps).
    If the TimeStampMode Registry entry is set to 3, it will default back to
    the TIMESTAMPMODE_SINGLE_SYNCHRONIZATION, which uses
    KeQueryPerformanceCounter() with similar characteristics to the RDTSC
    instruction.

--- packetWin7/npf/npf/time_calls.h
+++ packetWin7/npf/npf/time_calls.h
@@ -90,10 +90,10 @@
 
 #define TIMESTAMPMODE_SINGLE_SYNCHRONIZATION 0
 #define TIMESTAMPMODE_QUERYSYSTEMTIME 2
-#define TIMESTAMPMODE_RDTSC 3
 
 /* Deprecated modes, unsupported */
 #define TIMESTAMPMODE_SYNCHRONIZATION_ON_CPU_WITH_FIXUP 1
+#define TIMESTAMPMODE_RDTSC 3
 #define TIMESTAMPMODE_SYNCHRONIZATION_ON_CPU_NO_FIXUP 99
 
 extern ULONG g_TimestampMode;
@@ -159,132 +159,6 @@ __inline void SynchronizeOnCpu(struct timeval* start)
 	}
 }	
 
-//
-// inline assembler is not supported with the current AMD64 compilers
-// At the moment we simply disable this timestamping mode on AMD64.
-// A solution would be to allocate a small memory from the non-paged
-// pool, dump the instructions on that buffer, and then execute them.
-// The non paged pool is needed since it's the only area of kernel
-// data memory that is not subject to the NX protection.
-// Or use some lower level trick, like using an assembler to assemble
-// a small function for this. 
-//
-
-#ifdef _X86_
-/*RDTSC timestamps			*/
-/* callers must be at IRQL=PASSIVE_LEVEL*/
-__inline VOID TimeSynchronizeRDTSC(struct time_conv* data)
-{
-	struct timeval tmp;
-	LARGE_INTEGER system_time;
-	ULONGLONG curr_ticks;
-	KIRQL old;
-	LARGE_INTEGER start_kqpc, stop_kqpc, start_freq, stop_freq;
-	ULONGLONG start_ticks, stop_ticks;
-	ULONGLONG delta, delta2;
-	KEVENT event;
-	LARGE_INTEGER i;
-	ULONGLONG reference;
-
-	if (data->reference != 0)
-		return;
-
-	KeInitializeEvent(&event, NotificationEvent, FALSE);
-
-	i.QuadPart = -3500000;
-
-	KeRaiseIrql(HIGH_LEVEL, &old);
-	start_kqpc = KeQueryPerformanceCounter(&start_freq);
-	__asm
-	{
-	push eax
-	push edx
-	push ecx
-	rdtsc
-	lea ecx, start_ticks
-	mov[ecx + 4], edx
-	mov[ecx], eax
-	pop ecx
-	pop edx
-	pop eax
-	}
-
-	KeLowerIrql(old);
-
-	KeWaitForSingleObject(&event, UserRequest, KernelMode, TRUE, &i);
-
-	KeRaiseIrql(HIGH_LEVEL, &old);
-	stop_kqpc = KeQueryPerformanceCounter(&stop_freq);
-	__asm
-	{
-	push eax
-	push edx
-	push ecx
-	rdtsc
-	lea ecx, stop_ticks
-	mov[ecx + 4], edx
-	mov[ecx], eax
-	pop ecx
-	pop edx
-	pop eax
-	}
-	KeLowerIrql(old);
-
-	delta = stop_ticks - start_ticks;
-	delta2 = stop_kqpc.QuadPart - start_kqpc.QuadPart;
-	if (delta > 10000000000)
-	{
-		delta /= 16;
-		delta2 /= 16;
-	}
-
-	reference = delta * (start_freq.QuadPart) / delta2;
-
-	data->reference = reference / 1000;
-
-	if (reference % 1000 > 500)
-		data->reference++;
-
-	data->reference *= 1000;
-
-	reference = data->reference;
-
-	KeQuerySystemTime(&system_time);
-
-	__asm
-	{
-	push eax
-	push edx
-	push ecx
-	rdtsc
-	lea ecx, curr_ticks
-	mov[ecx + 4], edx
-	mov[ecx], eax
-	pop ecx
-	pop edx
-	pop eax
-	}
-
-	tmp.tv_sec = -(LONG)(curr_ticks / reference);
-
-	tmp.tv_usec = -(LONG)((curr_ticks % reference) * 1000000 / reference);
-
-	system_time.QuadPart -= 116444736000000000;
-
-	tmp.tv_sec += (LONG)(system_time.QuadPart / 10000000);
-	tmp.tv_usec += (LONG)((system_time.QuadPart % 10000000) / 10);
-
-	if (tmp.tv_usec < 0)
-	{
-		tmp.tv_sec--;
-		tmp.tv_usec += 1000000;
-	}
-
-	data->start = tmp;
-
-	IF_LOUD(DbgPrint("Frequency %I64u MHz\n", data->reference);)
-}
-#endif //_X86_
 
 #pragma optimize ("g",on)  //Due to some weird behaviour of the optimizer of DDK build 2600 
 
@@ -301,16 +175,6 @@ __inline VOID TIME_SYNCHRONIZE(struct time_conv* data)
 		data->reference = 1;
 	}
 	else
-		//
-		// This timestamp mode is supported on x86 (32 bit) only
-		//
-		#ifdef _X86_
-	if (g_TimestampMode == TIMESTAMPMODE_RDTSC)
-	{
-		TimeSynchronizeRDTSC(data);
-	}
-	else
-		#endif // _X86_
 	{
 		//it should be only the normal case i.e. TIMESTAMPMODE_SINGLESYNCHRONIZATION
 		SynchronizeOnCpu(&data->start);
@@ -341,55 +205,6 @@ __inline void GetTimeKQPC(struct timeval* dst, struct time_conv* data)
 	}
 }
 
-//
-// inline assembler is not supported with the current AMD64 compilers
-// At the moment we simply disable this timestamping mode on AMD64.
-// A solution would be to allocate a small memory from the non-paged
-// pool, dump the instructions on that buffer, and then execute them.
-// The non paged pool is needed since it's the only area of kernel
-// data memory that is not subject to the NX protection.
-// Or use some lower level trick, like using an assembler to assemble
-// a small function for this. 
-//
-
-#ifdef _X86_
-__inline void GetTimeRDTSC(struct timeval* dst, struct time_conv* data)
-{
-	ULONGLONG tmp = 0;
-	__asm
-	{
-		push eax
-		push edx
-		push ecx
-		rdtsc
-		lea ecx, tmp
-		mov [ecx+4], edx
-		mov [ecx], eax
-		pop ecx
-		pop edx
-		pop eax
-	}
-
-	if (data->reference == 0)
-	{
-		return;
-	}
-	dst->tv_sec = (LONG)(tmp / data->reference);
-
-	dst->tv_usec = (LONG)((tmp - dst->tv_sec * data->reference) * 1000000 / data->reference);
-
-	dst->tv_sec += data->start.tv_sec;
-
-	dst->tv_usec += data->start.tv_usec;
-
-	if (dst->tv_usec >= 1000000)
-	{
-		dst->tv_sec++;
-		dst->tv_usec -= 1000000;
-	}
-}
-#endif //_X86_
-
 __inline void GetTimeQST(struct timeval* dst, struct time_conv* data)
 {
 	LARGE_INTEGER SystemTime;
@@ -406,16 +221,6 @@ __inline void GetTimeQST(struct timeval* dst, struct time_conv* data)
 
 __inline void GET_TIME(struct timeval* dst, struct time_conv* data)
 {
-	//
-	// This timestamp mode is supported on x86 (32 bit) only
-	//
-#ifdef _X86_
-	if (g_TimestampMode == TIMESTAMPMODE_RDTSC)
-	{
-		GetTimeRDTSC(dst, data);
-	}
-	else
-		#endif // _X86_
 	if (g_TimestampMode == TIMESTAMPMODE_QUERYSYSTEMTIME)
 	{
 		GetTimeQST(dst, data);
