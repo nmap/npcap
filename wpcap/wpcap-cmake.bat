2532553e698f2f81525e0233019bb8bd0af9a376
Daniel Miller <dmiller@nmap.org>
2020-07-23 23:43:06 -0500

    Update cmake locations for libpcap/wpcap build

--- wpcap/wpcap-cmake.bat
+++ wpcap/wpcap-cmake.bat
@@ -1,13 +1,13 @@
-set CMAKE="C:\Program Files\CMake\bin\cmake.exe"
-set GENERATOR=Visual Studio 14 2015
+set CMAKE="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe"
+set GENERATOR=Visual Studio 16 2019
 set NPCAP_SDK=..\..\npcap-sdk-1.05\
 
 mkdir build-win32
 cd build-win32
-%CMAKE% -DPACKET_DLL_DIR=..\%NPCAP_SDK% -DLIBRARY_NAME=wpcap -G "%GENERATOR%" ..\libpcap\
+%CMAKE% -A Win32 -DPACKET_DLL_DIR=..\%NPCAP_SDK% -DLIBRARY_NAME=wpcap -G "%GENERATOR%" ..\libpcap\
 cd ..
 
 mkdir build-x64
 cd build-x64
-%CMAKE% -DPACKET_DLL_DIR=..\%NPCAP_SDK% -DLIBRARY_NAME=wpcap -G "%GENERATOR% Win64" ..\libpcap\
+%CMAKE% -A x64 -DPACKET_DLL_DIR=..\%NPCAP_SDK% -DLIBRARY_NAME=wpcap -G "%GENERATOR%" ..\libpcap\
 cd ..
