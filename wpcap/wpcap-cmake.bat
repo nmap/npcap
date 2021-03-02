c3c1acee5c802365476881d63a904b18f7a6015b
Daniel Miller <dmiller@nmap.org>
2021-03-02 15:29:21 -0600

    Base wpcap.dll on libpcap 1.10

--- wpcap/wpcap-cmake.bat
+++ wpcap/wpcap-cmake.bat
@@ -1,13 +1,14 @@
 set CMAKE="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe"
 set GENERATOR=Visual Studio 16 2019
-set NPCAP_SDK=..\..\npcap-sdk-1.05\
+set NPCAP_SDK=..\..\npcap-sdk-1.06
+set AIRPCAP_SDK=..\..\Airpcap_Devpack
 
 mkdir build-win32
 cd build-win32
-%CMAKE% -A Win32 -DPACKET_DLL_DIR=..\%NPCAP_SDK% -DLIBRARY_NAME=wpcap -G "%GENERATOR%" ..\libpcap\
+%CMAKE% -A Win32 -DPacket_ROOT=..\%NPCAP_SDK% -DLIBRARY_NAME=wpcap -DAirPcap_ROOT=..\%AIRPCAP_SDK% -G "%GENERATOR%" ..\libpcap\
 cd ..
 
 mkdir build-x64
 cd build-x64
-%CMAKE% -A x64 -DPACKET_DLL_DIR=..\%NPCAP_SDK% -DLIBRARY_NAME=wpcap -G "%GENERATOR%" ..\libpcap\
+%CMAKE% -A x64 -DPacket_ROOT=..\%NPCAP_SDK% -DLIBRARY_NAME=wpcap -DAirPcap_ROOT=..\%AIRPCAP_SDK% -G "%GENERATOR%" ..\libpcap\
 cd ..
