ebf99f1d1be1e680b6d44cd1a59e7b23b552f251
Daniel Miller <dmiller@nmap.org>
2021-10-06 13:38:16 -0500

    Upgrade tests build to VS16

--- build_test.bat
+++ build_test.bat
@@ -1,19 +1,27 @@
 SET MODE="Release"
 SET TOPSRCDIR=%cd%
+SET VERB=Build
+if NOT "%1" == "" SET VERB="%1"
 
-call build_sdk.bat || goto :error
-
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-
+rem call build_sdk.bat || goto :error
 cd %TOPSRCDIR%
-msbuild ".\npcap-sdk\Examples-pcap\MakeAll.sln" /m /t:Build /p:Configuration=%MODE% /p:Platform="x86" || goto :error
-msbuild ".\npcap-sdk\Examples-pcap\MakeAll.sln" /m /t:Build /p:Configuration=%MODE% /p:Platform="x64" || goto :error
-msbuild ".\npcap-sdk\Examples-remote\MakeAll.sln" /m /t:Build /p:Configuration=%MODE% /p:Platform="x86" || goto :error
-msbuild ".\npcap-sdk\Examples-remote\MakeAll.sln" /m /t:Build /p:Configuration=%MODE% /p:Platform="x64" || goto :error
+
+for /f "usebackq delims=#" %%a in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -version 16 -property installationPath`) do call "%%a\VC\Auxiliary\Build\vcvarsall.bat" x64
+if %ERRORLEVEL% NEQ 0 goto :error
+
+msbuild ".\npcap-sdk\Examples-pcap\MakeAll.sln" /m /t:%VERB% /p:Configuration=%MODE% /p:Platform="x64" || goto :error
+msbuild ".\npcap-sdk\Examples-remote\sendcap\sendcap.vcxproj" /m /t:%VERB% /p:Configuration=%MODE% /p:Platform="x64" || goto :error
+if NOT "%VERB%" == "Build" exit /b
 
 copy /b ".\npcap-sdk\Examples-pcap\x64\%MODE%\iflist.exe" test\
 copy /b ".\npcap-sdk\Examples-pcap\x64\%MODE%\pcap_filter.exe" test\
 copy /b ".\npcap-sdk\Examples-pcap\x64\%MODE%\sendpack.exe" test\
 copy /b ".\npcap-sdk\Examples-pcap\x64\%MODE%\readfile.exe" test\
 
-copy /b ".\npcap-sdk\Examples-remote\x64\%MODE%\sendcap.exe" test\
+copy /b ".\npcap-sdk\Examples-remote\sendcap\x64\%MODE%\sendcap.exe" test\
+
+exit /b
+
+:error
+echo Something failed: %ERRORLEVEL%
+exit /b 1
