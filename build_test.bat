042c4046c07357c0f99a3f12e2b9c1192a0376dd
Daniel Miller <dmiller@nmap.org>
2021-11-15 15:10:58 -0600

    Build 32-bit tests, not x86_64, so they run on all platforms.

--- build_test.bat
+++ build_test.bat
@@ -6,19 +6,19 @@ if NOT "%1" == "" SET VERB="%1"
 rem call build_sdk.bat || goto :error
 cd %TOPSRCDIR%
 
-for /f "usebackq delims=#" %%a in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -version 16 -property installationPath`) do call "%%a\VC\Auxiliary\Build\vcvarsall.bat" x64
+for /f "usebackq delims=#" %%a in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -version 16 -property installationPath`) do call "%%a\VC\Auxiliary\Build\vcvarsall.bat" x86
 if %ERRORLEVEL% NEQ 0 goto :error
 
-msbuild ".\npcap-sdk\Examples-pcap\MakeAll.sln" /m /t:%VERB% /p:Configuration=%MODE% /p:Platform="x64" || goto :error
-msbuild ".\npcap-sdk\Examples-remote\sendcap\sendcap.vcxproj" /m /t:%VERB% /p:Configuration=%MODE% /p:Platform="x64" || goto :error
+msbuild ".\npcap-sdk\Examples-pcap\MakeAll.sln" /m /t:%VERB% /p:Configuration=%MODE% /p:Platform="x86" || goto :error
+msbuild ".\npcap-sdk\Examples-remote\sendcap\sendcap.vcxproj" /m /t:%VERB% /p:Configuration=%MODE% /p:Platform="x86" || goto :error
 if NOT "%VERB%" == "Build" exit /b
 
-copy /b ".\npcap-sdk\Examples-pcap\x64\%MODE%\iflist.exe" test\
-copy /b ".\npcap-sdk\Examples-pcap\x64\%MODE%\pcap_filter.exe" test\
-copy /b ".\npcap-sdk\Examples-pcap\x64\%MODE%\sendpack.exe" test\
-copy /b ".\npcap-sdk\Examples-pcap\x64\%MODE%\readfile.exe" test\
+copy /b ".\npcap-sdk\Examples-pcap\%MODE%\iflist.exe" test\
+copy /b ".\npcap-sdk\Examples-pcap\%MODE%\pcap_filter.exe" test\
+copy /b ".\npcap-sdk\Examples-pcap\%MODE%\sendpack.exe" test\
+copy /b ".\npcap-sdk\Examples-pcap\%MODE%\readfile.exe" test\
 
-copy /b ".\npcap-sdk\Examples-remote\sendcap\x64\%MODE%\sendcap.exe" test\
+copy /b ".\npcap-sdk\Examples-remote\sendcap\%MODE%\sendcap.exe" test\
 
 exit /b
 
