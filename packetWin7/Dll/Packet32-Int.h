1582a81f3c96a487f42adb40668e15e46849d42c
Daniel Miller <dmiller@nmap.org>
2022-05-19 19:56:07 -0500

    Reapply updated license template to source files

--- packetWin7/Dll/Packet32-Int.h
+++ packetWin7/Dll/Packet32-Int.h
@@ -1,61 +1,75 @@
 /***********************IMPORTANT NPCAP LICENSE TERMS***********************
- *                                                                         *
- * Npcap (https://npcap.com) is the Nmap Project's packet capture (and     *
- * transmission) library for Microsoft Windows. It is (c) 2013-2022 by     *
- * Nmap Software LLC ("The Nmap Project"). All rights reserved.            *
- *                                                                         *
- * Even though Npcap source code is publicly available for review, it is   *
- * not open source software and the free version may not be redistributed  *
- * or incorporated into other software without special permission from     *
- * the Nmap Project. It also has certain usage limitations described in    *
- * the LICENSE file included with Npcap and also available at              *
- * https://github.com/nmap/npcap/blob/master/LICENSE. This header          *
- * summarizes a few important aspects of the Npcap license, but is not a   *
- * substitute for that full Npcap license agreement.                       *
- *                                                                         *
- * We fund the Npcap project by selling two commercial licenses:           *
- *                                                                         *
- * The Npcap OEM Redistribution License allows companies distribute Npcap  *
- * OEM within their products. Licensees generally use the Npcap OEM        *
- * silent installer, ensuring a seamless experience for end                *
- * users. Licensees may choose between a perpetual unlimited license or    *
- * an annual term license, along with options for commercial support and   *
- * updates. Prices and details: https://npcap.com/oem/redist.html          *
- *                                                                         *
- * The Npcap OEM Internal-Use License is for organizations that wish to    *
- * use Npcap OEM internally, without redistribution outside their          *
- * organization. This allows them to bypass the 5-system usage cap of the  *
- * Npcap free edition. It includes commercial support and update options,  *
- * and provides the extra Npcap OEM features such as the silent installer  *
- * for automated deployment. Prices and details:                           *
- * https://npcap.com/oem/internal.html                                     *
- *                                                                         *
- * Free and open source software producers are also welcome to contact us  *
- * for redistribution requests, but we normally recommend that such        *
- * authors instead ask their users to download and install Npcap           *
- * themselves.                                                             *
- *                                                                         *
- * Since the Npcap source code is available for download and review,       *
- * users sometimes contribute code patches to fix bugs or add new          *
- * features.  You are encouraged to submit such patches as Github pull     *
- * requests or by email to fyodor@nmap.org.  If you wish to specify        *
- * special license conditions or restrictions on your contributions, just  *
- * say so when you send them. Otherwise, it is understood that you are     *
- * offering the Nmap Project the unlimited, non-exclusive right to reuse,  *
- * modify, and relicense your code contributions so that we may (but are   *
- * not obligated to) incorporate them into Npcap.                          *
- *                                                                         *
- * This software is distributed in the hope that it will be useful, but    *
- * WITHOUT ANY WARRANTY; without even the implied warranty of              *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Warranty rights    *
- * and commercial support are available for the OEM Edition described      *
- * above.                                                                  *
- *                                                                         *
- * Other copyright notices and attribution may appear below this license   *
- * header. We have kept those for attribution purposes, but any license    *
- * terms granted by those notices apply only to their original work, and   *
- * not to any changes made by the Nmap Project or to this entire file.     *
- *                                                                         *
+ *
+ * Npcap (https://npcap.com) is a Windows packet sniffing driver and library
+ * and is copyright (c) 2013-2022 by Nmap Software LLC ("The Nmap Project").
+ * All rights reserved.
+ *
+ * Even though Npcap source code is publicly available for review, it
+ * is not open source software and may not be redistributed or used in
+ * other software without special permission from the Nmap
+ * Project. The standard (free) version is usually limited to
+ * installation on five systems. For more details, see the LICENSE
+ * file included with Npcap and also avaialble at
+ * https://github.com/nmap/npcap/blob/master/LICENSE. This header file
+ * summarizes a few important aspects of the Npcap license, but is not
+ * a substitute for that full Npcap license agreement.
+ *
+ * We fund the Npcap project by selling two types of commercial licenses to a
+ * special Npcap OEM edition:
+ *
+ * 1) The Npcap OEM Redistribution License allows companies distribute Npcap
+ * OEM within their products. Licensees generally use the Npcap OEM silent
+ * installer, ensuring a seamless experience for end users. Licensees may
+ * choose between a perpetual unlimited license or a quarterly term license,
+ * along with options for commercial support and updates. Prices and details:
+ * https://npcap.com/oem/redist.html
+ *
+ * 2) The Npcap OEM Internal-Use License is for organizations that wish to
+ * use Npcap OEM internally, without redistribution outside their
+ * organization. This allows them to bypass the 5-system usage cap of the
+ * Npcap free edition. It includes commercial support and update options, and
+ * provides the extra Npcap OEM features such as the silent installer for
+ * automated deployment. Prices and details:
+ * https://npcap.com/oem/internal.html
+ *
+ * Both of these licenses include updates and support as well as a
+ * warranty. Npcap OEM also includes a silent installer for unattended
+ * installation. Further details about Npcap OEM are available from
+ * https://npcap.com/oem/, and you are also welcome to contact us at
+ * sales@nmap.com to ask any questions or set up a license for your
+ * organization.
+ *
+ * Free and open source software producers are also welcome to contact us for
+ * redistribution requests. However, we normally recommend that such authors
+ * instead ask your users to download and install Npcap themselves. It will
+ * be free for them if they need 5 or fewer copies.
+ *
+ * If the Nmap Project (directly or through one of our commercial
+ * licensing customers) has granted you additional rights to Npcap or
+ * Npcap OEM, those additional rights take precedence where they
+ * conflict with the terms of the license agreement.
+ *
+ * Since the Npcap source code is available for download and review, users
+ * sometimes contribute code patches to fix bugs or add new features.  By
+ * sending these changes to the Nmap Project (including through direct email
+ * or our mailing lists or submitting pull requests through our source code
+ * repository), it is understood unless you specify otherwise that you are
+ * offering the Nmap Project the unlimited, non-exclusive right to reuse,
+ * modify, and relicense your code contribution so that we may (but are not
+ * obligated to) incorporate it into Npcap.  If you wish to specify special
+ * license conditions or restrictions on your contributions, just say so when
+ * you send them.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. Warranty rights and commercial
+ * support are available for the OEM Edition described above.
+ *
+ * Other copyright notices and attribution may appear below this license
+ * header. We have kept those for attribution purposes, but any license terms
+ * granted by those notices apply only to their original work, and not to any
+ * changes made by the Nmap Project or to this entire file.
+ *
  ***************************************************************************/
 /*
  * Copyright (c) 2006 - 2010
