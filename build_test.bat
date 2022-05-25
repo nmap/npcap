30fced274751d17fc17d005ed088d2b0ff22870f
Daniel Miller <dmiller@nmap.org>
2022-05-25 14:36:27 -0500

    Build test bins from source tree, not directly in SDK.

--- build_test.bat
+++ build_test.bat
@@ -17,20 +17,20 @@ if "%1" == "ARM64" set TOOLSET=amd64_arm64
 for /f "usebackq delims=#" %%a in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -version 16 -property installationPath`) do call "%%a\VC\Auxiliary\Build\vcvarsall.bat" %TOOLSET%
 if %ERRORLEVEL% NEQ 0 goto :error
 
-msbuild /p:ForceImportBeforeCppTargets="%CD%\test\static.props" ".\npcap-sdk\Examples-pcap\MakeAll.sln" /m /t:%VERB% /p:Configuration=%MODE% /p:Platform="%1" || goto :error
-msbuild /p:ForceImportBeforeCppTargets="%CD%\test\static.props" ".\npcap-sdk\Examples-remote\sendcap\sendcap.vcxproj" /m /t:%VERB% /p:Configuration=%MODE% /p:Platform="%1" || goto :error
+msbuild /p:ForceImportBeforeCppTargets="%CD%\test\static.props" ".\Examples-pcap\MakeAll.sln" /m /t:%VERB% /p:Configuration=%MODE% /p:Platform="%1" || goto :error
+msbuild /p:ForceImportBeforeCppTargets="%CD%\test\static.props" ".\Examples\sendcap\sendcap.vcxproj" /m /t:%VERB% /p:Configuration=%MODE% /p:Platform="%1" || goto :error
 if NOT "%VERB%" == "Build" goto :EOF
 
 set BINDIR=%1\
 if "%1" == "x86" set BINDIR=""
 
 mkdir test\%1\
-copy /b ".\npcap-sdk\Examples-pcap\%BINDIR%%MODE%\iflist.exe" test\%1\
-copy /b ".\npcap-sdk\Examples-pcap\%BINDIR%%MODE%\pcap_filter.exe" test\%1\
-copy /b ".\npcap-sdk\Examples-pcap\%BINDIR%%MODE%\sendpack.exe" test\%1\
-copy /b ".\npcap-sdk\Examples-pcap\%BINDIR%%MODE%\readfile.exe" test\%1\
+copy /b ".\Examples-pcap\%BINDIR%%MODE%\iflist.exe" test\%1\
+copy /b ".\Examples-pcap\%BINDIR%%MODE%\pcap_filter.exe" test\%1\
+copy /b ".\Examples-pcap\%BINDIR%%MODE%\sendpack.exe" test\%1\
+copy /b ".\Examples-pcap\%BINDIR%%MODE%\readfile.exe" test\%1\
 
-copy /b ".\npcap-sdk\Examples-remote\sendcap\%BINDIR%%MODE%\sendcap.exe" test\%1\
+copy /b ".\Examples\sendcap\%BINDIR%%MODE%\sendcap.exe" test\%1\
 
 goto :EOF
 
