5b180ab6b7b855eac44f4ba5eec4124ccbe7508f
Daniel Miller <dmiller@nmap.org>
2022-05-25 14:41:48 -0500

    Use git archive to make a clean copy of examples for SDK

--- create_examples.bat
+++ create_examples.bat
@@ -1,23 +1,27 @@
 @echo off
 
-IF "%2"=="" (set WPDPACKDESTDIR=.\npcap-sdk\) ELSE (set WPDPACKDESTDIR=%2)
+IF "%2"=="" (set WPDPACKDESTDIR=npcap-sdk) ELSE (set WPDPACKDESTDIR=%2)
 
 IF ""=="%1" (set WINPCAPSOURCEDIR=.\) ELSE (set WINPCAPSOURCEDIR=%1) 
 
 echo Creating \Examples folder
 mkdir %WPDPACKDESTDIR% >nul 2>nul
+rd /S /Q %WPDPACKDESTDIR%\Examples-pcap >nul 2>nul
 mkdir %WPDPACKDESTDIR%\Examples-pcap >nul 2>nul
+rd /S /Q %WPDPACKDESTDIR%\Examples-remote >nul 2>nul
 mkdir %WPDPACKDESTDIR%\Examples-remote >nul 2>nul
 
-git clean -fxd %WINPCAPSOURCEDIR%\Examples
-git clean -fxd %WINPCAPSOURCEDIR%\Examples-pcap
+rem Can't pipe stdout to stdin of tar; claims "Damaged archive"
+git archive --prefix="%WPDPACKDESTDIR%/Examples-remote/" HEAD:Examples -o Examples-remote.tar
+tar xf Examples-remote.tar
+del Examples-remote.tar
 
-xcopy /s/e/v /Y %WINPCAPSOURCEDIR%\Examples		%WPDPACKDESTDIR%\Examples-remote >nul
- 
+git archive --prefix="%WPDPACKDESTDIR%/Examples-pcap/" HEAD:Examples-pcap -o Examples-pcap.tar
+tar xf Examples-pcap.tar
+del Examples-pcap.tar
+
 rem *** Delete Netmeter since it's no more part of the Developer's pack *** 
 rd /S /Q %WPDPACKDESTDIR%\Examples-remote\NetMeter\
-
-xcopy /s/e/v /Y %WINPCAPSOURCEDIR%\Examples-pcap		%WPDPACKDESTDIR%\Examples-pcap >nul
 
 rem *** Delete WinPcapStress, since it's not a real example ***
 rd /S /Q %WPDPACKDESTDIR%\Examples-pcap\winpcap_stress
