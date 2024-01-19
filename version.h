a3d510b8f18c465ce6719df2546c71c083cd7149
Daniel Miller <dmiller@nmap.com>
2024-01-19 12:16:25 -0800

    Bump version and changelog for 1.79 release

--- version.h
+++ version.h
@@ -110,11 +110,11 @@
  !define /**/ WINPCAP_MINOR				1
 /* WINPCAP_REV should be less than 256 to fit in UCHAR */
 #define /*
- !define /**/ WINPCAP_REV				78
+ !define /**/ WINPCAP_REV				79
 #define /*
- !define /**/ WINPCAP_BUILD				1018
+ !define /**/ WINPCAP_BUILD				117
 #define /*
- !define /**/ WINPCAP_VER_STRING		"1.78"
+ !define /**/ WINPCAP_VER_STRING		"1.79"
 #define /*
  !define /**/ NPCAP_SDK_VERSION "1.14"
 
