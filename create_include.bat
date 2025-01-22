3fadf26c77953965984697e55912961120fa54bc
Daniel Miller <bonsaiviking@gmail.com>
2025-01-22 11:15:34 -0600

    Include npcap-bpf.h and npcap-defs.h in SDK

--- create_include.bat
+++ create_include.bat
@@ -10,16 +10,22 @@ mkdir %WPDPACKDESTDIR%\Include  	2>nul >nul
 mkdir %WPDPACKDESTDIR%\Include\pcap  	2>nul >nul
 
 
-rem xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\pcap\*.h		 		%WPDPACKDESTDIR%\Include\pcap\	>nul
-
 SETLOCAL ENABLEEXTENSIONS
-
-for /F "usebackq tokens=2* delims==" %%i in (`C:\cygwin\bin\make.exe -p -q -f Makefile.in -C "%WINPCAPSOURCEDIR%\wpcap\libpcap" ^| findstr /b "PUBHDR"`) do set PUBHDR=%%i
+for /F "usebackq tokens=*" %%m in (`where.exe make`) do set MAKE=%%m
+if not exist "%MAKE%" set MAKE=C:\cygwin\bin\make.exe
+if not exist "%MAKE%" set MAKE=C:\msys64\usr\bin\make.exe
+if not exist "%MAKE%" (
+	echo "No make found"
+	exit 1
+)
+for /F "usebackq tokens=2* delims==" %%i in (`%MAKE% -p -q -f Makefile.in -C "%WINPCAPSOURCEDIR%\wpcap\libpcap" ^| findstr /b "PUBHDR"`) do set PUBHDR=%%i
 
 for %%i in (%PUBHDR:/=\%) do copy /v /Y "%WINPCAPSOURCEDIR%\wpcap\libpcap\%%i" "%WPDPACKDESTDIR%\Include\%%i"
 
 
-xcopy /v /Y %WINPCAPSOURCEDIR%\Common\Packet32.h			 	%WPDPACKDESTDIR%\Include\	>nul
+xcopy /v /Y %WINPCAPSOURCEDIR%\Common\Packet32.h   %WPDPACKDESTDIR%\Include\ >nul
+xcopy /v /Y %WINPCAPSOURCEDIR%\Common\npcap-bpf.h  %WPDPACKDESTDIR%\Include\ >nul
+xcopy /v /Y %WINPCAPSOURCEDIR%\Common\npcap-defs.h %WPDPACKDESTDIR%\Include\ >nul
 
 echo Folder \Include created successfully
 set WPDPACKDESTDIR=
