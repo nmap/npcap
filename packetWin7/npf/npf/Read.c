2c274563475e0b8975cd719207b347c9070e22cf
Daniel Miller <bonsaiviking@gmail.com>
2026-09-11 16:46:08 -0500

    Fix operator precedence mixup

--- packetWin7/npf/npf/Read.c
+++ packetWin7/npf/npf/Read.c
@@ -1035,7 +1035,7 @@ NPF_TapEx(
 	{
 #endif
 		/* Track only receives that will result in a call to NPF_ReturnEx */
-		if (0 == ReceiveFlags & NDIS_RECEIVE_FLAGS_RESOURCES) {
+		if (0 == (ReceiveFlags & NDIS_RECEIVE_FLAGS_RESOURCES)) {
 			NPF_TrackReceives(pFiltMod, NumberOfNetBufferLists, bAtDispatchLevel);
 		}
 		NdisFIndicateReceiveNetBufferLists(
